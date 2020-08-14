a8d0e656ccaf593d23912bebd36406eb7d343509
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-14 02:27:38 +0200

    func(localization): create localizer and abstractions

---
+++ pkg/localization/localizer.go
@@ -0,0 +1,114 @@
+package localization
+
+type (
+	// Config is a data struct that contains all information needed to create
+	// a localized message.
+	Config struct {
+		// Term is the key of the translation.
+		Term string
+		// Placeholders are the filled placeholders of the translation.
+		Placeholders Placeholders
+		// Plural is a number or a string containing such, that is used to
+		// identify if the message should be pluralized or not.
+		//
+		// If nil, the other message should be used.
+		Plural interface{}
+
+		// Fallback is the fallback used if the LangFunc is nil or the
+		// LangFunc returned an error.
+		Fallback Fallback
+	}
+
+	// Placeholders is the type used for placeholders.
+	Placeholders map[string]interface{}
+
+	// Fallback is the English fallback used if a translation is not available.
+	// The message is created using go's text/template system and left and
+	// right delimiters are {{ and }} respectively.
+	Fallback struct {
+		// One is the singular form of the fallback message, if there is any.
+		One string
+		// Other is the plural form of the fallback message.
+		// This is also the default form, meaning if no pluralization is needed
+		// this field should be used.
+		Other string
+	}
+)
+
+// genTranslation attempts to generate the translation using the passed
+// Placeholders and the passed plural data.
+func (f Fallback) genTranslation(placeholders Placeholders, plural interface{}) (string, error) {
+	if plural != nil { // we have plural information
+		if isOne, err := isOne(plural); err != nil { // attempt to check if plural is == 1
+			return "", err
+		} else if isOne {
+			s, err := fillTemplate(f.One, placeholders)
+			return s, err
+		}
+	}
+
+	// no plural information or plural was != 1
+
+	s, err := fillTemplate(f.Other, placeholders)
+	return s, err
+}
+
+// Localizer is a translator for a specific language.
+// It provides multiple utility functions and wraps a LangFunc.
+type Localizer struct {
+	// f is the LangFunc used to create translations.
+	f LangFunc
+}
+
+// Localize generates a localized message using the passed config.
+// c.Term must be set.
+func (l *Localizer) Localize(c Config) (s string, err error) {
+	if l.f != nil { // try the user-defined translator first, if there is one
+		s, err = l.f(c.Term, c.Placeholders, c.Plural)
+		if err == nil {
+			return
+		}
+	}
+
+	// otherwise use fallback if there is;
+	// checking other suffices as it will always be set if there is a fallback
+	if c.Fallback.Other == "" {
+		return c.Term, &NoTranslationGeneratedError{
+			Term: c.Term,
+		}
+	}
+
+	s, err = c.Fallback.genTranslation(c.Placeholders, c.Plural)
+	if err != nil {
+		return c.Term, err
+	}
+
+	return
+}
+
+// LocalizeTerm is a short for
+//		l.Localize(localization.Config{
+//			Term: term,
+//		})
+func (l *Localizer) LocalizeTerm(term string) (string, error) { return l.Localize(Config{Term: term}) }
+
+// MustLocalize is the same as Localize, but it panics if there is an error.
+func (l *Localizer) MustLocalize(c Config) string {
+	s, err := l.Localize(c)
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
+
+// MustLocalizeTerm is the same as LocalizeTerm, but it panics if there is an
+// error.
+func (l *Localizer) MustLocalizeTerm(term string) string {
+	s, err := l.LocalizeTerm(term)
+	if err != nil {
+		panic(err)
+	}
+
+	return s
+}
---
+++ pkg/localization/localizer_test.go
@@ -0,0 +1,296 @@
+package localization
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFallback_genTranslation(t *testing.T) {
+	const (
+		expectOne   = "abc"
+		expectOther = "def"
+	)
+
+	successCases := []struct {
+		name string
+
+		plural interface{}
+		expect string
+	}{
+		{
+			name:   "plural is 1",
+			plural: 1,
+			expect: expectOne,
+		},
+		{
+			name:   "plural not 1",
+			plural: 0,
+			expect: expectOther,
+		},
+		{
+			name:   "no plural",
+			plural: nil,
+			expect: expectOther,
+		},
+	}
+
+	t.Run("success", func(t *testing.T) {
+		for _, c := range successCases {
+			t.Run(c.name, func(t *testing.T) {
+				f := Fallback{
+					One:   expectOne,
+					Other: expectOther,
+				}
+
+				actual, err := f.genTranslation(nil, c.plural)
+				require.NoError(t, err)
+				assert.Equal(t, c.expect, actual)
+			})
+		}
+	})
+
+	failureCases := []struct {
+		name       string
+		one, other string
+		plural     interface{}
+	}{
+		{
+			name:   "invalid plural type",
+			plural: []int{1, 2, 3},
+		},
+		{
+			name:   "invalid one template",
+			one:    "{{{.Error}}",
+			plural: 1,
+		},
+		{
+			name:   "invalid other template",
+			one:    "",
+			other:  "{{{.Error}}",
+			plural: nil,
+		},
+	}
+
+	t.Run("failure", func(t *testing.T) {
+		for _, c := range failureCases {
+			t.Run(c.name, func(t *testing.T) {
+				f := Fallback{
+					One:   c.one,
+					Other: c.other,
+				}
+
+				_, err := f.genTranslation(nil, c.plural)
+				assert.Error(t, err)
+			})
+		}
+	})
+}
+
+func TestLocalizer_Localize(t *testing.T) {
+	successCases := []struct {
+		name     string
+		langFunc LangFunc
+		config   Config
+		expect   string
+	}{
+		{
+			name: "lang func",
+			langFunc: func(term string, placeholders Placeholders, plural interface{}) (string, error) {
+				if term != "abc" {
+					panic(fmt.Sprint("unexpected term: ", term))
+				}
+
+				if !reflect.DeepEqual(placeholders, Placeholders{"def": "ghi"}) {
+					panic(fmt.Sprint("unexpected placeholders: ", placeholders))
+				}
+
+				if plural != "jkl" {
+					panic(fmt.Sprint("unexpected plural: ", plural))
+				}
+
+				return "abc", nil
+			},
+			config: Config{
+				Term:         "abc",
+				Placeholders: Placeholders{"def": "ghi"},
+				Plural:       "jkl",
+			},
+			expect: "abc",
+		},
+		{
+			name:     "fallback",
+			langFunc: nil,
+			config: Config{
+				Fallback: Fallback{
+					Other: "abc",
+				},
+			},
+			expect: "abc",
+		},
+	}
+
+	t.Run("success", func(t *testing.T) {
+		for _, c := range successCases {
+			t.Run(c.name, func(t *testing.T) {
+				l := c.langFunc.Localizer()
+
+				actual, err := l.Localize(c.config)
+				require.NoError(t, err)
+				assert.Equal(t, c.expect, actual)
+			})
+		}
+	})
+
+	failureCases := []struct {
+		name     string
+		langFunc LangFunc
+		config   Config
+	}{
+		{
+			name: "lang func error",
+			langFunc: func(string, Placeholders, interface{}) (string, error) {
+				return "", errors.New("something went wrong")
+			},
+		},
+		{
+			name:     "no lang func and fallback",
+			langFunc: nil,
+		},
+		{
+			name:     "fallback error",
+			langFunc: nil,
+			config: Config{
+				Fallback: Fallback{
+					Other: "{{{.Error}}",
+				},
+			},
+		},
+	}
+
+	t.Run("failure", func(t *testing.T) {
+		for _, c := range failureCases {
+			t.Run(c.name, func(t *testing.T) {
+				l := c.langFunc.Localizer()
+
+				if c.config.Term == "" {
+					c.config.Term = "term"
+				}
+
+				actual, err := l.Localize(c.config)
+				assert.Equal(t, c.config.Term, actual)
+				assert.Error(t, err)
+			})
+		}
+	})
+}
+
+func TestLocalizer_LocalizeTerm(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		term := "abc"
+
+		expect := "def"
+
+		l := LangFunc(func(t string, placeholders Placeholders, plural interface{}) (string, error) {
+			if t != term {
+				panic(fmt.Sprint("unexpected term: ", term))
+			}
+
+			if placeholders != nil {
+				panic(fmt.Sprint("unexpected placeholders: ", placeholders))
+			}
+
+			if plural != nil {
+				panic(fmt.Sprint("unexpected plural: ", plural))
+			}
+
+			return expect, nil
+		}).Localizer()
+
+		actual, err := l.LocalizeTerm(term)
+		require.NoError(t, err)
+		assert.Equal(t, expect, actual)
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		term := "abc"
+
+		l := LangFunc(nil).Localizer()
+
+		actual, err := l.LocalizeTerm(term)
+		assert.Equal(t, term, actual)
+		assert.True(t, errors.Is(err, &NoTranslationGeneratedError{
+			Term: term,
+		}), "unexpected error")
+	})
+}
+
+func TestLocalizer_MustLocalize(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		expect := "abc"
+
+		l := LangFunc(nil).Localizer()
+
+		var actual string
+
+		require.NotPanics(t, func() {
+			actual = l.MustLocalize(Config{
+				Fallback: Fallback{
+					Other: expect,
+				},
+			})
+		})
+		assert.Equal(t, expect, actual)
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		l := LangFunc(nil).Localizer()
+
+		assert.Panics(t, func() {
+			l.MustLocalize(Config{})
+		})
+	})
+}
+
+func TestLocalizer_MustLocalizeTerm(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		term := "abc"
+
+		expect := "def"
+
+		l := LangFunc(func(t string, placeholders Placeholders, plural interface{}) (string, error) {
+			if t != term {
+				panic(fmt.Sprint("unexpected term: ", term))
+			}
+
+			if placeholders != nil {
+				panic(fmt.Sprint("unexpected placeholders: ", placeholders))
+			}
+
+			if plural != nil {
+				panic(fmt.Sprint("unexpected plural: ", plural))
+			}
+
+			return expect, nil
+		}).Localizer()
+
+		var actual string
+
+		assert.NotPanics(t, func() {
+			actual = l.MustLocalizeTerm(term)
+		})
+		assert.Equal(t, expect, actual)
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		l := LangFunc(nil).Localizer()
+
+		assert.Panics(t, func() {
+			l.MustLocalizeTerm("abc")
+		})
+	})
+}
