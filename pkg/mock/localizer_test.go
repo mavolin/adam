a96aa3c55cb6cce51a175fa231207415fe8de3cc
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-15 22:39:50 +0200

    feat(mock): add Localizer mock

---
+++ pkg/mock/localizer_test.go
@@ -0,0 +1,56 @@
+package mock
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLocalizer_Clone(t *testing.T) {
+	l1 := NewLocalizer().
+		On("abc", "def")
+
+	l2 := l1.Clone()
+
+	assert.Equal(t, l1, l2)
+
+	l1.On("ghi", "jkl")
+
+	assert.NotEqual(t, l1, l2)
+}
+
+func TestLocalizer_Build(t *testing.T) {
+	t.Run("default", func(t *testing.T) {
+		expect := "abc"
+
+		l := NewLocalizerWithDefault(expect).
+			Build()
+
+		actual, err := l.LocalizeTerm("")
+		assert.NoError(t, err)
+		assert.Equal(t, expect, actual)
+	})
+
+	t.Run("expected localization", func(t *testing.T) {
+		term := "abc"
+
+		expect := "def"
+
+		l := NewLocalizer().
+			On(term, expect).
+			Build()
+
+		actual, err := l.LocalizeTerm(term)
+		assert.NoError(t, err)
+		assert.Equal(t, expect, actual)
+	})
+
+	t.Run("unexpected localization", func(t *testing.T) {
+		l := NewLocalizer().
+			Build()
+
+		assert.Panics(t, func() {
+			l.LocalizeTerm("abc")
+		})
+	})
+}
