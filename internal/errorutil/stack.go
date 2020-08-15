e7ebf1b1978d76360edc9edecf19c095f4355d23
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-15 23:13:17 +0200

    feat(errorutil): create StackTrace

---
+++ internal/errorutil/stack.go
@@ -0,0 +1,18 @@
+package errorutil
+
+import "runtime"
+
+// stackDepth is the maximum depth of a Stack.
+const stackDepth = 32
+
+// Stack is a collection of program counters that show the stack trace of an
+// error.
+type Stack []uintptr
+
+// GenerateStackTrace generates a Stack.
+func GenerateStackTrace() Stack {
+	var pcs []uintptr
+
+	n := runtime.Callers(3, pcs)
+	return pcs[0:n]
+}
