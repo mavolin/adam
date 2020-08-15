67a87eb45b82668ca771ca32c4a8f48122038e65
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-15 21:33:12 +0200

    feat(plugin): add plugin abstractions

---
+++ pkg/plugin/module.go
@@ -0,0 +1,61 @@
+package plugin
+
+import (
+	"github.com/diamondburned/arikawa/discord"
+
+	"github.com/mavolin/adam/pkg/localization"
+)
+
+type (
+	// Module is the abstraction of a module.
+	//
+	// A default for a simple module can be found in impl/module.
+	Module interface {
+		// GetMeta returns the meta information of the module.
+		Meta() ModuleMeta
+
+		// Commands returns the subcommands of the module.
+		Commands() []Command
+		// Modules returns the submodules of the module.
+		Modules() []Module
+	}
+
+	// ModuleMeta is the abstraction of the Module's meta data.
+	//
+	// Default implementations can be found in impl/module.
+	ModuleMeta interface {
+		// GetName returns the name of the module
+		GetName() string
+		// GetShortDescription returns an optional one-sentence description of
+		// the module.
+		GetShortDescription(l *localization.Localizer) (string, error)
+		// GetLongDescription returns an option long description of the module.
+		GetLongDescription(l *localization.Localizer) (string, error)
+		// IsHidden specifies whether this module will be hidden from the help
+		// page.
+		//
+		// If set to true, all submodules and subcommands will be hidden as
+		// well.
+		IsHidden() bool
+		// GetChannelType returns the ChannelType required to use this module.
+		//
+		// Commands can overwrite this, by setting a custom ChannelType.
+		GetChannelType() ChannelType
+		// GetBotPermissions get the permissions needed to use this module.
+		//
+		// Commands can overwrite this, by setting custom BotPermissions.
+		GetBotPermissions() discord.Permissions
+		// IsRestricted checks if the user calling the command is restricted
+		// from using this module.
+		//
+		// Commands can overwrite this, by returning a non-nil RestrictionFunc.
+		GetRestrictionFunc() RestrictionFunc
+		// GetThrottling returns the ThrottlingOptions for the module.
+		// This defines how often all commands and submodules in this module
+		// together may be used.
+		//
+		// If either of the fields in ThrottlingOptions is zero value, the
+		// module won't be throttled.
+		GetThrottling() ThrottlingOptions
+	}
+)
