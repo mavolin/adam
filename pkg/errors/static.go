3bb2ffe3aa818ae9f18be83298ee20c2aa681bf2
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-16 14:53:11 +0200

    feat(errors): use noHandlingError for Abort

--- pkg/errors/static.go
+++ pkg/errors/static.go
@@ -1,9 +1,26 @@
 package errors
 
+import (
+	"github.com/mavolin/disstate/pkg/state"
+
+	"github.com/mavolin/adam/pkg/plugin"
+)
+
+// noHandlingError is an error that won't be handled.
+type noHandlingError struct {
+	s string
+}
+
+func (e *noHandlingError) Error() string                              { return e.s }
+func (e *noHandlingError) Is(err error) bool                          { return e == err }
+func (e *noHandlingError) Handle(*state.State, *plugin.Context) error { return nil }
+
 // Abort is similar to a break in a for-loop.
-// It stops the execution of a command silently, without producing neither a
+// It stops the execution of a command silently, while producing neither a
 // logged error nor a message to the calling user.
 //
 // It is intended to be used, if the user signals to cancel a command early
 // and is therefore just a signaling error, rather than an actual exception.
-var Abort = New("abort")
+var Abort = &noHandlingError{
+	s: "abort",
+}
