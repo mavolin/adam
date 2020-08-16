d7dc78cb07b7954edf89ea09d9591ec917139dc3
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-16 18:06:56 +0200

    docs(errors): add docs terms.go

--- pkg/errors/terms.go
+++ pkg/errors/terms.go
@@ -3,11 +3,14 @@ package errors
 import "github.com/mavolin/adam/pkg/localization"
 
 const (
-	// termError is the term used for
-	termError               = "errors.error"
+	// termError is the title of error embeds.
+	termError = "errors.error"
+	// termInternalDescription is the default description of an InternalError.
 	termInternalDescription = "errors.internal.description"
 )
 
+// errorTitleConfig is the localization.Config used to generate the title of an
+// error embed.
 var errorTitleConfig = localization.Config{
 	Term: termError,
 	Fallback: localization.Fallback{
