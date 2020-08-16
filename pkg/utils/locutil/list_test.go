0fa2da470a97f774af813adfd05e6ca3ebd863e6
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-16 23:12:57 +0200

    feat(locutil): add sorted options for lists

--- pkg/utils/locutil/list_test.go
+++ pkg/utils/locutil/list_test.go
@@ -24,6 +24,21 @@ func TestInterfacesToList(t *testing.T) {
 	assert.Equal(t, expect, actual)
 }
 
+func TestInterfacesToSortedList(t *testing.T) {
+	expect := "eggs, jam, milk and orange juice"
+
+	list := []interface{}{"milk", "eggs", "orange juice", "jam"}
+
+	l := mock.
+		NewLocalizer().
+		On("lang.lists.default_separator", ", ").
+		On("lang.lists.last_separator", " and ").
+		Build()
+
+	actual := InterfacesToSortedList(list, l)
+	assert.Equal(t, expect, actual)
+}
+
 func TestConfigsToList(t *testing.T) {
 	expect := "milk, eggs and orange juice"
 
@@ -47,6 +62,29 @@ func TestConfigsToList(t *testing.T) {
 	assert.Equal(t, expect, actual)
 }
 
+func TestConfigsToSortedList(t *testing.T) {
+	expect := "eggs, milk and orange juice"
+
+	list := []localization.Config{
+		localization.QuickConfig("milk"),
+		localization.QuickConfig("eggs"),
+		localization.QuickConfig("orange_juice"),
+	}
+
+	l := mock.
+		NewLocalizer().
+		On("lang.lists.default_separator", ", ").
+		On("lang.lists.last_separator", " and ").
+		On("milk", "milk").
+		On("eggs", "eggs").
+		On("orange_juice", "orange juice").
+		Build()
+
+	actual, err := ConfigsToSortedList(list, l)
+	assert.NoError(t, err)
+	assert.Equal(t, expect, actual)
+}
+
 func TestTermsToList(t *testing.T) {
 	expect := "milk, eggs and orange juice"
 
@@ -63,5 +101,33 @@ func TestTermsToList(t *testing.T) {
 
 	actual, err := TermsToList(list, l)
 	assert.NoError(t, err)
+	assert.Equal(t, expect, actual)
+}
+
+func TestTermsToSortedList(t *testing.T) {
+	expect := "eggs, milk and orange juice"
+
+	list := []string{"milk", "eggs", "orange_juice"}
+
+	l := mock.
+		NewLocalizer().
+		On("lang.lists.default_separator", ", ").
+		On("lang.lists.last_separator", " and ").
+		On("milk", "milk").
+		On("eggs", "eggs").
+		On("orange_juice", "orange juice").
+		Build()
+
+	actual, err := TermsToSortedList(list, l)
+	assert.NoError(t, err)
+	assert.Equal(t, expect, actual)
+}
+
+func Test_stringsToSortedList(t *testing.T) {
+	expect := "abc, def, ghi and jkl"
+
+	list := []string{"def", "jkl", "ghi", "abc"}
+
+	actual := stringsToSortedList(list, ", ", " and ")
 	assert.Equal(t, expect, actual)
 }
