6d3332643187b0f9c70056a63ff185a0aa37e45e
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-18 04:17:20 +0200

    feat(plugin): add ReplyXDM methods to Context

--- pkg/plugin/context_test.go
+++ pkg/plugin/context_test.go
@@ -85,6 +85,88 @@ func TestContext_Reply(t *testing.T) {
 	m.Eval()
 }
 
+func TestContext_Replyl(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	var (
+		term    localization.Term = "abc"
+		content                   = "def"
+	)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					ChannelID: 123,
+				},
+			},
+		},
+		Localizer: mock.NewLocalizer().
+			On(term, content).
+			Build(),
+		s: s,
+	}
+
+	expect := &discord.Message{
+		ID: 123,
+		Author: discord.User{
+			ID: 456,
+		},
+		ChannelID: ctx.ChannelID,
+		Content:   content,
+	}
+
+	m.SendText(*expect)
+
+	actual, err := ctx.Replyl(localization.Config{
+		Term: term,
+	})
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_Replylt(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	var (
+		term    localization.Term = "abc"
+		content                   = "def"
+	)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					ChannelID: 123,
+				},
+			},
+		},
+		Localizer: mock.NewLocalizer().
+			On(term, content).
+			Build(),
+		s: s,
+	}
+
+	expect := &discord.Message{
+		ID: 123,
+		Author: discord.User{
+			ID: 456,
+		},
+		ChannelID: ctx.ChannelID,
+		Content:   content,
+	}
+
+	m.SendText(*expect)
+
+	actual, err := ctx.Replylt(term)
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
 func TestContext_ReplyEmbed(t *testing.T) {
 	m, s := state.NewMocker(t)
 
@@ -212,88 +294,6 @@ func TestContext_ReplyLocalizedEmbedBuilder(t *testing.T) {
 	m.Eval()
 }
 
-func TestContext_Replyl(t *testing.T) {
-	m, s := state.NewMocker(t)
-
-	var (
-		term    localization.Term = "abc"
-		content                   = "def"
-	)
-
-	ctx := &Context{
-		MessageCreateEvent: &state.MessageCreateEvent{
-			MessageCreateEvent: &gateway.MessageCreateEvent{
-				Message: discord.Message{
-					ChannelID: 123,
-				},
-			},
-		},
-		Localizer: mock.NewLocalizer().
-			On(term, content).
-			Build(),
-		s: s,
-	}
-
-	expect := &discord.Message{
-		ID: 123,
-		Author: discord.User{
-			ID: 456,
-		},
-		ChannelID: ctx.ChannelID,
-		Content:   content,
-	}
-
-	m.SendText(*expect)
-
-	actual, err := ctx.Replyl(localization.Config{
-		Term: term,
-	})
-	require.NoError(t, err)
-	assert.Equal(t, expect, actual)
-
-	m.Eval()
-}
-
-func TestContext_Replylt(t *testing.T) {
-	m, s := state.NewMocker(t)
-
-	var (
-		term    localization.Term = "abc"
-		content                   = "def"
-	)
-
-	ctx := &Context{
-		MessageCreateEvent: &state.MessageCreateEvent{
-			MessageCreateEvent: &gateway.MessageCreateEvent{
-				Message: discord.Message{
-					ChannelID: 123,
-				},
-			},
-		},
-		Localizer: mock.NewLocalizer().
-			On(term, content).
-			Build(),
-		s: s,
-	}
-
-	expect := &discord.Message{
-		ID: 123,
-		Author: discord.User{
-			ID: 456,
-		},
-		ChannelID: ctx.ChannelID,
-		Content:   content,
-	}
-
-	m.SendText(*expect)
-
-	actual, err := ctx.Replylt(term)
-	require.NoError(t, err)
-	assert.Equal(t, expect, actual)
-
-	m.Eval()
-}
-
 func TestContext_ReplyMessage(t *testing.T) {
 	m, s := state.NewMocker(t)
 
@@ -320,6 +320,337 @@ func TestContext_ReplyMessage(t *testing.T) {
 	m.SendText(*expect)
 
 	actual, err := ctx.ReplyMessage(api.SendMessageData{
+		Content: expect.Content,
+	})
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_ReplyDM(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					Author: discord.User{
+						ID: 123,
+					},
+				},
+			},
+		},
+		s: s,
+	}
+
+	var channelID discord.ChannelID = 456
+
+	expect := &discord.Message{
+		ID: 789,
+		Author: discord.User{
+			ID: 123,
+		},
+		ChannelID: channelID,
+		Content:   "abc",
+	}
+
+	m.CreatePrivateChannel(discord.Channel{
+		ID:           channelID,
+		DMRecipients: []discord.User{{ID: ctx.Author.ID}},
+	})
+	m.SendText(*expect)
+
+	actual, err := ctx.ReplyDM(expect.Content)
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_ReplyDMl(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	var (
+		term    localization.Term = "abc"
+		content                   = "def"
+	)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					Author: discord.User{
+						ID: 123,
+					},
+				},
+			},
+		},
+		Localizer: mock.NewLocalizer().
+			On(term, content).
+			Build(),
+		s: s,
+	}
+
+	var channelID discord.ChannelID = 456
+
+	expect := &discord.Message{
+		ID: 789,
+		Author: discord.User{
+			ID: 123,
+		},
+		ChannelID: channelID,
+		Content:   content,
+	}
+
+	m.CreatePrivateChannel(discord.Channel{
+		ID:           channelID,
+		DMRecipients: []discord.User{{ID: ctx.Author.ID}},
+	})
+	m.SendText(*expect)
+
+	actual, err := ctx.ReplyDMl(localization.Config{
+		Term: term,
+	})
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_ReplyDMlt(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	var (
+		term    localization.Term = "abc"
+		content                   = "def"
+	)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					Author: discord.User{
+						ID: 123,
+					},
+				},
+			},
+		},
+		Localizer: mock.NewLocalizer().
+			On(term, content).
+			Build(),
+		s: s,
+	}
+
+	var channelID discord.ChannelID = 456
+
+	expect := &discord.Message{
+		ID: 789,
+		Author: discord.User{
+			ID: 123,
+		},
+		ChannelID: channelID,
+		Content:   content,
+	}
+
+	m.CreatePrivateChannel(discord.Channel{
+		ID:           channelID,
+		DMRecipients: []discord.User{{ID: ctx.Author.ID}},
+	})
+	m.SendText(*expect)
+
+	actual, err := ctx.ReplyDMlt(term)
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_ReplyEmbedDM(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					Author: discord.User{
+						ID: 123,
+					},
+				},
+			},
+		},
+		s: s,
+	}
+
+	var channelID discord.ChannelID = 456
+
+	expect := &discord.Message{
+		ID: 789,
+		Author: discord.User{
+			ID: 123,
+		},
+		ChannelID: channelID,
+		Content:   "abc",
+		Embeds: []discord.Embed{
+			{
+				Type:  discord.NormalEmbed,
+				Color: discord.DefaultEmbedColor,
+			},
+		},
+	}
+
+	m.CreatePrivateChannel(discord.Channel{
+		ID:           channelID,
+		DMRecipients: []discord.User{{ID: ctx.Author.ID}},
+	})
+	m.SendEmbed(*expect)
+
+	actual, err := ctx.ReplyEmbedDM(expect.Embeds[0])
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_ReplyEmbedBuilderDM(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					Author: discord.User{
+						ID: 123,
+					},
+				},
+			},
+		},
+		s: s,
+	}
+
+	builder := discordutil.
+		NewEmbedBuilder().
+		WithSimpleTitle("abc").
+		WithDescription("def").
+		WithColor(discord.DefaultEmbedColor)
+
+	embed := builder.Build()
+	embed.Type = discord.NormalEmbed
+
+	var channelID discord.ChannelID = 456
+
+	expect := &discord.Message{
+		ID: 789,
+		Author: discord.User{
+			ID: 123,
+		},
+		ChannelID: channelID,
+		Content:   "abc",
+		Embeds:    []discord.Embed{embed},
+	}
+
+	m.CreatePrivateChannel(discord.Channel{
+		ID:           channelID,
+		DMRecipients: []discord.User{{ID: ctx.Author.ID}},
+	})
+	m.SendEmbed(*expect)
+
+	actual, err := ctx.ReplyEmbedBuilderDM(builder)
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_ReplyLocalizedEmbedBuilderDM(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	l := mock.NewLocalizerWithDefault("abc").Build()
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					Author: discord.User{
+						ID: 123,
+					},
+				},
+			},
+		},
+		Localizer: l,
+		s:         s,
+	}
+
+	builder := discordutil.
+		NewLocalizedEmbedBuilder().
+		WithSimpleTitlet("abc").
+		WithDescriptiont("def").
+		WithColor(discord.DefaultEmbedColor)
+
+	embed, err := builder.Build(l)
+	require.NoError(t, err)
+
+	embed.Type = discord.NormalEmbed
+
+	var channelID discord.ChannelID = 456
+
+	expect := &discord.Message{
+		ID: 789,
+		Author: discord.User{
+			ID: 123,
+		},
+		ChannelID: channelID,
+		Content:   "abc",
+		Embeds:    []discord.Embed{embed},
+	}
+
+	m.CreatePrivateChannel(discord.Channel{
+		ID:           channelID,
+		DMRecipients: []discord.User{{ID: ctx.Author.ID}},
+	})
+	m.SendEmbed(*expect)
+
+	actual, err := ctx.ReplyLocalizedEmbedBuilderDM(builder)
+	require.NoError(t, err)
+	assert.Equal(t, expect, actual)
+
+	m.Eval()
+}
+
+func TestContext_ReplyMessageDM(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	ctx := &Context{
+		MessageCreateEvent: &state.MessageCreateEvent{
+			MessageCreateEvent: &gateway.MessageCreateEvent{
+				Message: discord.Message{
+					Author: discord.User{
+						ID: 123,
+					},
+				},
+			},
+		},
+		s: s,
+	}
+
+	var channelID discord.ChannelID = 456
+
+	expect := &discord.Message{
+		ID: 789,
+		Author: discord.User{
+			ID: 123,
+		},
+		ChannelID: channelID,
+		Content:   "abc",
+	}
+
+	m.CreatePrivateChannel(discord.Channel{
+		ID:           channelID,
+		DMRecipients: []discord.User{{ID: ctx.Author.ID}},
+	})
+	m.SendText(*expect)
+
+	actual, err := ctx.ReplyMessageDM(api.SendMessageData{
 		Content: expect.Content,
 	})
 	require.NoError(t, err)
