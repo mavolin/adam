c59dc69f321d19d09df33195db0c8348d8a672de
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-26 22:41:07 +0200

    feat(errors): add InvalidChannelTypeError

---
+++ pkg/errors/invalid_channel_type_error_test.go
@@ -0,0 +1,72 @@
+package errors
+
+import (
+	"testing"
+
+	"github.com/diamondburned/arikawa/discord"
+	"github.com/diamondburned/arikawa/gateway"
+	"github.com/mavolin/disstate/pkg/state"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/mavolin/adam/pkg/mock"
+	"github.com/mavolin/adam/pkg/plugin"
+)
+
+func TestInvalidChannelError_Is(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		types := plugin.GuildChannels
+
+		err1 := NewInvalidChannelTypeError(types)
+		err2 := NewInvalidChannelTypeError(types)
+
+		assert.True(t, err1.Is(err2))
+	})
+
+	t.Run("different types", func(t *testing.T) {
+		err1 := NewInvalidChannelTypeError(plugin.DirectMessages)
+		err2 := New("abc")
+
+		assert.False(t, err1.Is(err2))
+	})
+
+	t.Run("different missing permissions", func(t *testing.T) {
+		err1 := NewInvalidChannelTypeError(plugin.DirectMessages)
+		err2 := NewInvalidChannelTypeError(plugin.GuildChannels)
+
+		assert.False(t, err1.Is(err2))
+	})
+}
+
+func TestInvalidChannelTypeError_Handle(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	ctx := plugin.NewContext(s)
+	ctx.MessageCreateEvent = &state.MessageCreateEvent{
+		MessageCreateEvent: &gateway.MessageCreateEvent{
+			Message: discord.Message{
+				ChannelID: 123,
+			},
+		},
+	}
+	ctx.Localizer = mock.NewLocalizer().
+		On(errorTitle.Term, "title").
+		On(channelTypeErrorGuild.Term, "guild").
+		Build()
+
+	embed := newErrorEmbedBuilder(ctx.Localizer).
+		WithDescriptionl(channelTypeErrorGuild).
+		MustBuild(ctx.Localizer)
+
+	m.SendEmbed(discord.Message{
+		ChannelID: ctx.ChannelID,
+		Embeds:    []discord.Embed{embed},
+	})
+
+	e := NewInvalidChannelTypeError(plugin.GuildChannels)
+
+	err := e.Handle(s, ctx)
+	require.NoError(t, err)
+
+	m.Eval()
+}
--- pkg/errors/terms.go
+++ pkg/errors/terms.go
@@ -22,9 +22,32 @@ var (
 		"It seems as if I don't have sufficient permissions to run this command. Please give me the following "+
 			"permissions and try again.")
 	insufficientBotPermissionMissingMissingPermissionsFieldName = localization.NewFallbackConfig(
-		"errors.insufficient_bot_permissions.missing_permissions.name", "Missing Permissions")
+		"errors.insufficient_bot_permissions.fields.missing_permissions.name",
+		"Missing Permissions")
 
 	argumentParsingReasonFieldName = localization.NewFallbackConfig("errors.argument_parsing.reason.name", "Reason")
+
+	channelTypeErrorGuildText = localization.NewFallbackConfig(
+		"errors.channel_type.description.guild_text",
+		"You must use this command in a regular text channel.")
+	channelTypeErrorGuildNews = localization.NewFallbackConfig(
+		"errors.channel_types.description.guild_news.",
+		"You must use this command in an announcement channel.")
+	channelTypeErrorDirectMessage = localization.NewFallbackConfig(
+		"errors.channel_types.description.direct_message.",
+		"You must use this command in a direct message.")
+	channelTypeErrorGuild = localization.NewFallbackConfig(
+		"errors.channel_types.description.guild.",
+		"You must use this command in a server.")
+	channelTypeErrorDirectMessageAndGuildText = localization.NewFallbackConfig(
+		"errors.channel_types.description.direct_message_and_guild_text.",
+		"You must use this command in a direct message or a regular text channel.")
+	channelTypeErrorDirectMessageAndGuildNews = localization.NewFallbackConfig(
+		"errors.channel_types.description.direct_message_and_guild_news.",
+		"You must use this command in a direct message or a announcement channel.")
+	channelTypeErrorFallback = localization.NewFallbackConfig(
+		"errors.channel_type.description.fallback",
+		"Ypu can't use this command in this type of channel.")
 )
 
 type insufficientBotPermissionsDescSinglePlaceholders struct {
