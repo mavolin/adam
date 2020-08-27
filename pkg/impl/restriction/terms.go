62935e827761ed6d7c8cabca74b0571fa7bb3a69
mavolin <48887425+mavolin@users.noreply.github.com>
2020-08-27 22:23:01 +0200

    feat(restriction): use list for roles

--- pkg/impl/restriction/terms.go
+++ pkg/impl/restriction/terms.go
@@ -16,4 +16,30 @@ var (
 	allMessageInline = localization.NewFallbackConfig(
 		"restrictions.compare.all.inline",
 		"You need to fulfill all of these requirements:")
+
+	notNSFWChannelError = localization.NewFallbackConfig(
+		"restrictions.nsfw.errors.not_nsfw.description",
+		"This command must be invoked in an NSFW channel.")
+
+	notOwnerError = localization.NewFallbackConfig(
+		"restrictions.owner.errors.not_owner.description",
+		"You must be the owner of the server to invoke this command.")
+
+	notBotOwnerError = localization.NewFallbackConfig(
+		"restrictions.bot_owner.errors.not_bot_owner.description",
+		"You must be the owner of the bot to invoke this command.")
+
+	missingRoleError = localization.NewFallbackConfig(
+		"restrictions.roles.errors.missing_role",
+		"You need the {{.role_name}} role to use this command.")
+	missingRolesAllError = localization.NewFallbackConfig(
+		"restrictions.roles.errors.missing_roles.all",
+		"You need these roles to use this command:")
+	missingRolesAnyError = localization.NewFallbackConfig(
+		"restrictions.roles.errors.missing_roles.any",
+		"You need at least one of these roles to use this command:")
 )
+
+type missingRoleErrorPlaceholders struct {
+	RoleName string
+}
