9c0247c4323b42d3b80ba545061e79bb08e74d27
mavolin <48887425+mavolin@users.noreply.github.com>
2020-09-09 20:43:10 +0200

    feat(plugin): update Provider definition

--- pkg/plugin/context.go
+++ pkg/plugin/context.go
@@ -197,30 +197,34 @@ type (
 	// Copies are only created on call of one of the methods.
 	Provider interface {
 		// AllCommands returns a copy of all top-level commands.
-		// Commands[0] contains the built-in commands of the bot, or is nil if
-		// there are none, all following outer slices contain runtime commands.
+		// Commands[0] contains the built-in commands of the bot, and is named
+		// 'built_in'.
+		// If there are no built-in top-level commands, Commands will be nil.
 		//
 		// AllCommands will always return valid data, even if error != nil.
-		// However, all runtime commands, whose providers returned an error,
-		// won't be included, and their errors will be returned wrapped in a
-		// bot.RuntimePluginProviderError.
-		// If multiple errors occur, a errors.MultiError filled with
-		// bot.RuntimePluginProviderErrors will be returned.
-		AllCommands() ([][]Command, error)
+		// If a bot.RuntimePluginProvider returns an error, it will be wrapped
+		// in a bot.RuntimePluginProviderError, that contains the name of the
+		// runtime plugin provider and the original error.
+		//
+		// If multiple errors occur, the error will be of type
+		// errors.MultiError.
+		AllCommands() ([]CommandRepository, error)
 		// AllModules returns a copy of all top-level modules.
-		// Modules[0] contains the built-in modules of the bot, or is nil if
-		// there are none, all following outer slices contain runtime commands.
+		// Modules[0] contains the built-in commands of the bot, and is named
+		// 'built_in'.
+		// If there are no built-in top-level modules, Modules will be nil.
 		//
 		// AllModules will always return valid data, even if error != nil.
-		// However, all runtime modules, whose providers returned an error,
-		// won't be included, and their error will be returned wrapped in a
-		// bot.RuntimePluginProviderError.
-		// If multiple errors occur, a errors.MultiError filled with
-		// bot.RuntimePluginProviderErrors will be returned.
-		AllModules() ([][]Module, error)
+		// If a bot.RuntimePluginProvider returns an error, it will be wrapped
+		// in a bot.RuntimePluginProviderError that contains the name of the
+		// runtime plugin provider and the original error.
+		//
+		// If multiple errors occur, the error will be of type
+		// errors.MultiError.
+		AllModules() ([]ModuleRepository, error)
 
-		// Commands returns merged version of AllCommands, as the command
-		// router uses it.
+		// Commands returns merged version of AllCommands for simpler
+		// iteration.
 		//
 		// Commands will always return valid data, even if error != nil.
 		// However, all runtime commands, whose providers returned an error,
@@ -232,8 +236,9 @@ type (
 		// Modules returns a merged version of AllModules, as the command
 		// router uses it.
 		//
-		// Modules may not be the original modules, as they might need to store
-		// data from other modules as well.
+		// Some Modules might not be the original module but a wrapper, since
+		// runtime modules might extend this module and merging becomes
+		// necessary.
 		//
 		// Modules will always return valid data, even if error != nil.
 		// However, all runtime modules, whose providers returned an error,
@@ -250,14 +255,18 @@ type (
 		//
 		// It will return nil, nil if no command matching the identifier was
 		// found.
-		// An error will only be returned if one of the runtime plugin
-		// providers returns an error, and should therefore be seen as an
-		// indicator that the command may exist, but is unavailable right now.
+		// An error will only be returned if one of the
+		// bot.RuntimePluginProviders returns an error, and should therefore
+		// be seen as an indicator that the command may exist, but is
+		// unavailable right now.
 		// If so, it will be wrapped in a bot.RuntimePluginProviderError.
 		// If multiple errors occur, the returned error will be of type
 		// errors.MultiError.
 		Command(Identifier) (Command, error)
 		// Module returns the Module with the passed Identifier.
+		//
+		// This might not be the original module but a wrapper, since runtime
+		// modules might extend this module and merging becomes necessary.
 		//
 		// It will return nil, nil if no module matching the identifier was
 		// found.
@@ -285,6 +294,9 @@ type (
 		FindCommand(invoke string) (Command, error)
 		// FindModule returns the Module with the passed invoke.
 		//
+		// This might not be the original module but a wrapper, since runtime
+		// modules might extend this module and merging becomes necessary.
+		//
 		// It will return nil, nil if no module matching the passed invoke was
 		// found.
 		// An error will only be returned if one of the runtime plugin
@@ -296,6 +308,26 @@ type (
 		FindModule(invoke string) (Module, error)
 	}
 
+	// ModuleRepository is the struct returned by Provider.AllModules.
+	ModuleRepository struct {
+		// Name is the name of the bot.RuntimePluginProvider that provides
+		// these modules.
+		Name string
+		// Modules are the modules were returned by the
+		// bot.RuntimePluginProvider.
+		Modules []Module
+	}
+
+	// CommandRepository is the struct returned by Provider.AllCommands.
+	CommandRepository struct {
+		// Name is the name of the bot.RuntimePluginProvider that provides
+		// these commands.
+		Name string
+		// Commands are the commands that were returned by the
+		// bot.RuntimePluginProvider.
+		Commands []Command
+	}
+
 	// ErrorHandler is an embeddable interface used to provide direct error
 	// handling capabilities to a command.
 	// This is useful if an error is encountered, that should be captured
@@ -303,8 +335,8 @@ type (
 	ErrorHandler interface {
 		// HandleError hands the error to the bot's error handler.
 		HandleError(err interface{})
-		// HandleErrorSilent wraps the error using errors.Silent and hands it to
-		// the bot's error handler.
+		// HandleErrorSilent wraps the error using errors.Silent and hands it
+		// to the bot's error handler.
 		HandleErrorSilent(err interface{})
 	}
 )
