3543002363748966a307e53b13270dc6f0924c55
mavolin <48887425+mavolin@users.noreply.github.com>
2020-09-22 19:30:07 +0200

    feat(error): expose noHandlingError as InformationalError

---
+++ pkg/errors/informational_error.go
@@ -0,0 +1,29 @@
+package errors
+
+import (
+	"github.com/mavolin/disstate/pkg/state"
+
+	"github.com/mavolin/adam/pkg/plugin"
+)
+
+// Abort is similar to a break in a for-loop.
+// It stops the execution of a command silently, while producing neither a
+// logged error nor a message to the calling user.
+//
+// It is intended to be used, if the user signals to cancel a command early
+// and is therefore just a signaling error, rather than an actual exception.
+var Abort error = &InformationalError{
+	s: "abort",
+}
+
+// InformationalError is an error that won't be handled.
+// It is used to communicate preliminary stop of execution without signaling
+// an actual error.
+// See Abort for an example.
+type InformationalError struct {
+	s string
+}
+
+func (e *InformationalError) Error() string                              { return e.s }
+func (e *InformationalError) Is(err error) bool                          { return e == err }
+func (e *InformationalError) Handle(*state.State, *plugin.Context) error { return nil }
