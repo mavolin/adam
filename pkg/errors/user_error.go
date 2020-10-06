d162011b02fc2ad8572b9b1d0498b153dc56edd2
mavolin <48887425+mavolin@users.noreply.github.com>
2020-10-06 22:08:11 +0200

    feat(errors): add full embed functionality to UserError

--- pkg/errors/user_error.go
+++ pkg/errors/user_error.go
@@ -7,27 +7,22 @@ import (
 	"github.com/mavolin/adam/pkg/localization"
 	"github.com/mavolin/adam/pkg/plugin"
 	"github.com/mavolin/adam/pkg/utils/embedutil"
-	"github.com/mavolin/adam/pkg/utils/locutil"
 )
 
 // UserError is an error on the user-side.
 // The user will reported via a message containing a detailed description of
 // the problem.
-// The error won't be logged or captured by sentry.
+// The error won't be logged.
 type UserError struct {
-	// description of the error
-	desc locutil.Text
-
-	// used just for fields
-	fields *embedutil.Builder
+	embed *embedutil.Builder
 }
 
 // NewUserError creates a new UserError with the passed description.
 // The description mustn't be empty for this error to be handled properly.
 func NewUserError(description string) *UserError {
 	return &UserError{
-		desc:   locutil.NewStaticText(description),
-		fields: embedutil.NewBuilder(),
+		embed: ErrorEmbed.Clone().
+			WithDescription(description),
 	}
 }
 
@@ -35,8 +30,8 @@ func NewUserError(description string) *UserError {
 // passed localization.Config as description.
 func NewUserErrorl(description localization.Config) *UserError {
 	return &UserError{
-		desc:   locutil.NewLocalizedText(description),
-		fields: embedutil.NewBuilder(),
+		embed: ErrorEmbed.Clone().
+			WithDescriptionl(description),
 	}
 }
 
@@ -46,12 +41,200 @@ func NewUserErrorlt(description localization.Term) *UserError {
 	return NewUserErrorl(description.AsConfig())
 }
 
+// WithSimpleTitle adds a plain title (max. 256 characters) to the error.
+func (e *UserError) WithSimpleTitle(title string) *UserError {
+	e.embed.WithSimpleTitle(title)
+	return e
+}
+
+// WithSimpleTitlel adds a plain title (max. 256 characters) to the error.
+func (e *UserError) WithSimpleTitlel(title localization.Config) *UserError {
+	e.embed.WithSimpleTitlel(title)
+	return e
+}
+
+// WithSimpleTitlelt adds a plain title (max. 256 characters) to the error.
+func (e *UserError) WithSimpleTitlelt(title localization.Term) *UserError {
+	return e.WithSimpleTitlel(title.AsConfig())
+}
+
+// WithTitle adds a title (max. 256 characters) with a link to the error.
+func (e *UserError) WithTitle(title string, url discord.URL) *UserError {
+	e.embed.WithTitle(title, url)
+	return e
+}
+
+// WithTitlel adds a title (max. 256 characters) with a link to the error.
+func (e *UserError) WithTitlel(title localization.Config, url discord.URL) *UserError {
+	e.embed.WithTitlel(title, url)
+	return e
+}
+
+// WithTitlelt adds a title (max. 256 characters) with a link to the error.
+func (e *UserError) WithTitlelt(title localization.Term, url discord.URL) *UserError {
+	return e.WithTitlel(title.AsConfig(), url)
+}
+
+// WithDescription adds a description (max. 2048 characters) to the error.
+func (e *UserError) WithDescription(description string) *UserError {
+	e.embed.WithDescription(description)
+	return e
+}
+
+// WithDescriptionl adds a description (max. 2048 characters) to the error.
+func (e *UserError) WithDescriptionl(description localization.Config) *UserError {
+	e.embed.WithDescriptionl(description)
+	return e
+}
+
+// WithDescriptionlt adds a description (max. 2048 characters) to the error.
+func (e *UserError) WithDescriptionlt(description localization.Term) *UserError {
+	return e.WithDescriptionl(description.AsConfig())
+}
+
+// WithTimestamp adds a discord.Timestamp to the error.
+func (e *UserError) WithTimestamp(timestamp discord.Timestamp) *UserError {
+	e.embed.WithTimestamp(timestamp)
+	return e
+}
+
+// WithTimestamp adds a timestamp of the current time to the error.
+func (e *UserError) WithTimestampNow() *UserError {
+	return e.WithTimestamp(discord.NowTimestamp())
+}
+
+// WithColor sets the color of the embed to the passed discord.Color.
+func (e *UserError) WithColor(color discord.Color) *UserError {
+	e.embed.WithColor(color)
+	return e
+}
+
+// WithSimpleFooter adds a plain footer (max. 2048 characters) to the error.
+func (e *UserError) WithSimpleFooter(text string) *UserError {
+	e.embed.WithSimpleFooter(text)
+	return e
+}
+
+// WithSimpleFooterl adds a plain footer (max. 2048 characters) to the error.
+func (e *UserError) WithSimpleFooterl(text localization.Config) *UserError {
+	e.embed.WithSimpleFooterl(text)
+	return e
+}
+
+// WithSimpleFooterlt adds a plain footer (max. 2048 characters) to the error.
+func (e *UserError) WithSimpleFooterlt(text localization.Term) *UserError {
+	return e.WithSimpleFooterl(text.AsConfig())
+}
+
+// WithFooter adds a footer (max. 2048 character) with an icon to the error.
+func (e *UserError) WithFooter(text string, icon discord.URL) *UserError {
+	e.embed.WithField(text, icon)
+	return e
+}
+
+// WithFooterl adds a footer (max. 2048 character) with an icon to the error.
+func (e *UserError) WithFooterl(text localization.Config, icon discord.URL) *UserError {
+	e.embed.WithFooterl(text, icon)
+	return e
+}
+
+// WithFooterlt adds a footer (max. 2048 character) with an icon to the error.
+func (e *UserError) WithFooterlt(text localization.Term, icon discord.URL) *UserError {
+	return e.WithFooterl(text.AsConfig(), icon)
+}
+
+// WithImage adds an image to the error.
+func (e *UserError) WithImage(image discord.URL) *UserError {
+	e.embed.WithImage(image)
+	return e
+}
+
+// WithThumbnail adds a thumbnail to the error.
+func (e *UserError) WithThumbnail(thumbnail discord.URL) *UserError {
+	e.embed.WithThumbnail(thumbnail)
+	return e
+}
+
+// WithSimpleAuthor adds a plain author (max. 256 characters) to the error.
+func (e *UserError) WithSimpleAuthor(name string) *UserError {
+	e.embed.WithSimpleAuthor(name)
+	return e
+}
+
+// WithSimpleAuthorl adds a plain author (max. 256 characters) to the error.
+func (e *UserError) WithSimpleAuthorl(name localization.Config) *UserError {
+	e.embed.WithSimpleAuthorl(name)
+	return e
+}
+
+// WithSimpleAuthorlt adds a plain author (max. 256 characters) to the error.
+func (e *UserError) WithSimpleAuthorlt(name localization.Term) *UserError {
+	return e.WithSimpleAuthorl(name.AsConfig())
+}
+
+// WithSimpleAuthorWithURL adds an author (max. 256 character) with a URL to
+// the embed.
+func (e *UserError) WithSimpleAuthorWithURL(name string, url discord.URL) *UserError {
+	e.embed.WithSimpleAuthorWithURL(name, url)
+	return e
+}
+
+// WithSimpleAuthorWithURLl adds an author (max. 256 character) with a URL to
+// the embed.
+func (e *UserError) WithSimpleAuthorWithURLl(name localization.Config, url discord.URL) *UserError {
+	e.embed.WithSimpleAuthorWithURLl(name, url)
+	return e
+}
+
+// WithSimpleAuthorWithURLlt adds an author (max. 256 character) with a URL to
+// the embed.
+func (e *UserError) WithSimpleAuthorWithURLlt(name localization.Term, url discord.URL) *UserError {
+	return e.WithSimpleAuthorWithURLl(name.AsConfig(), url)
+}
+
+// WithAuthor adds an author (max 256 characters) with an icon to the error.
+func (e *UserError) WithAuthor(name string, icon discord.URL) *UserError {
+	e.embed.WithAuthor(name, icon)
+	return e
+}
+
+// WithAuthorl adds an author (max 256 characters) with an icon to the error.
+func (e *UserError) WithAuthorl(name localization.Config, icon discord.URL) *UserError {
+	e.embed.WithAuthorl(name, icon)
+	return e
+}
+
+// WithAuthorlt adds an author (max 256 characters) with an icon to the error.
+func (e *UserError) WithAuthorlt(name localization.Term, icon discord.URL) *UserError {
+	return e.WithAuthorl(name.AsConfig(), icon)
+}
+
+// WithAuthorWithURL adds an author (max 256 characters) with an icon and a URL
+// to the error.
+func (e *UserError) WithAuthorWithURL(name string, icon, url discord.URL) *UserError {
+	e.embed.WithAuthorWithURL(name, icon, url)
+	return e
+}
+
+// WithAuthorWithURLl adds an author (max 256 characters) with an icon and a
+// URL to the error.
+func (e *UserError) WithAuthorWithURLl(name localization.Config, icon, url discord.URL) *UserError {
+	e.embed.WithAuthorWithURLl(name, icon, url)
+	return e
+}
+
+// WithAuthorWithURLlt adds an author (max 256 characters) with an icon and a
+// URL to the error.
+func (e *UserError) WithAuthorWithURLlt(name localization.Term, icon, url discord.URL) *UserError {
+	return e.WithAuthorWithURLl(name.AsConfig(), icon, url)
+}
+
 // WithField adds the passed field to the error, and returns a pointer to the
 // UserError to allow chaining.
 // Name or value may be empty, in which case the field won't have a name or
 // value.
 func (e *UserError) WithField(name, value string) *UserError {
-	e.fields.WithField(name, value)
+	e.embed.WithField(name, value)
 	return e
 }
 
@@ -60,7 +243,7 @@ func (e *UserError) WithField(name, value string) *UserError {
 // Name or value may be empty, in which case the field won't have a name or
 // value.
 func (e *UserError) WithFieldl(name, value localization.Config) *UserError {
-	e.fields.WithFieldl(name, value)
+	e.embed.WithFieldl(name, value)
 	return e
 }
 
@@ -77,7 +260,7 @@ func (e *UserError) WithFieldlt(name, value localization.Term) *UserError {
 // Name or value may be empty, in which case the field won't have a name or
 // value.
 func (e *UserError) WithInlinedField(name, value string) *UserError {
-	e.fields.WithInlinedField(name, value)
+	e.embed.WithInlinedField(name, value)
 	return e
 }
 
@@ -86,7 +269,7 @@ func (e *UserError) WithInlinedField(name, value string) *UserError {
 // Name or value may be empty, in which case the field won't have a name or
 // value.
 func (e *UserError) WithInlinedFieldl(name, value localization.Config) *UserError {
-	e.fields.WithInlinedFieldl(name, value)
+	e.embed.WithInlinedFieldl(name, value)
 	return e
 }
 
@@ -98,49 +281,15 @@ func (e *UserError) WithInlinedFieldlt(name, value localization.Term) *UserError
 	return e.WithFieldl(name.AsConfig(), value.AsConfig())
 }
 
-// Description returns the description of the error and localizes it, if
-// possible.
-// Name or value may be empty, in which case the field won't have a name or
-// value.
-func (e *UserError) Description(l *localization.Localizer) (string, error) {
-	return e.desc.Get(l)
-}
-
-// Fields returns the discord.EmbedFields of the UserError.
-// This can be safely ignored, if only used for errors generated by adam, as
-// these will never have fields.
-func (e *UserError) Fields(l *localization.Localizer) ([]discord.EmbedField, error) {
-	embed, err := e.fields.Build(l)
-	if err != nil {
-		return nil, err
-	}
-
-	return embed.Fields, nil
+// Embed returns the embed of the UserError.
+func (e *UserError) Embed(l *localization.Localizer) (discord.Embed, error) {
+	return e.embed.Build(l)
 }
 
 func (e *UserError) Error() string { return "user error" }
 
 // Handle sends an error embed with the description of the UserError.
-func (e *UserError) Handle(_ *state.State, ctx *plugin.Context) error {
-	desc, err := e.Description(ctx.Localizer)
-	if err != nil {
-		return err
-	}
-
-	embed, err := ErrorEmbed.Clone().
-		WithDescription(desc).
-		Build(ctx.Localizer)
-	if err != nil {
-		return err
-	}
-
-	fields, err := e.Fields(ctx.Localizer)
-	if err != nil {
-		return err
-	}
-
-	embed.Fields = fields
-
-	_, err = ctx.ReplyEmbed(embed)
-	return err
+func (e *UserError) Handle(_ *state.State, ctx *plugin.Context) (err error) {
+	_, err = ctx.ReplyEmbedBuilder(e.embed)
+	return
 }
--- pkg/errors/user_error_test.go
+++ pkg/errors/user_error_test.go
@@ -6,45 +6,14 @@ import (
 	"github.com/diamondburned/arikawa/discord"
 	"github.com/diamondburned/arikawa/gateway"
 	"github.com/mavolin/disstate/v2/pkg/state"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/mavolin/adam/pkg/localization"
 	"github.com/mavolin/adam/pkg/plugin"
 	"github.com/mavolin/adam/pkg/utils/mock"
 )
 
-func TestUserError_Description(t *testing.T) {
-	t.Run("string description", func(t *testing.T) {
-		expect := "abc"
-
-		e := NewUserError(expect)
-
-		actual, err := e.Description(mock.NoOpLocalizer)
-		require.NoError(t, err)
-		assert.Equal(t, expect, actual)
-	})
-
-	t.Run("localized description", func(t *testing.T) {
-		var term localization.Term = "abc"
-
-		expect := "def"
-
-		l := mock.
-			NewLocalizer(t).
-			On(term, expect).
-			Build()
-
-		e := NewUserErrorlt(term)
-
-		actual, err := e.Description(l)
-		require.NoError(t, err)
-		assert.Equal(t, expect, actual)
-	})
-}
-
 func TestUserError_Handle(t *testing.T) {
-	t.Run("without fields", func(t *testing.T) {
+	t.Run("without embed", func(t *testing.T) {
 		expectDesc := "abc"
 
 		m, s := state.NewMocker(t)
@@ -76,7 +45,7 @@ func TestUserError_Handle(t *testing.T) {
 		m.Eval()
 	})
 
-	t.Run("with fields", func(t *testing.T) {
+	t.Run("with embed", func(t *testing.T) {
 		var (
 			expectDesc       = "abc"
 			expectFieldName  = "def"
