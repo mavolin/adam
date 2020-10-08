b25384562bcb908780e74da1c04e1f6da8587bc9
mavolin <48887425+mavolin@users.noreply.github.com>
2020-10-08 18:07:28 +0200

    feat(bot): add MiddlewareManager

---
+++ pkg/bot/middleware.go
@@ -0,0 +1,146 @@
+package bot
+
+import (
+	"sync"
+
+	"github.com/mavolin/disstate/v2/pkg/state"
+
+	"github.com/mavolin/adam/pkg/errors"
+	"github.com/mavolin/adam/pkg/plugin"
+)
+
+var ErrNotAMiddleware = errors.New("the passed func does not resemble a valid middleware")
+
+type (
+	// CommandFunc is the Invoke function of a plugin.Command.
+	CommandFunc func(s *state.State, ctx *plugin.Context) (interface{}, error)
+	// MiddlewareFunc is the function of a middleware.
+	MiddlewareFunc func(next CommandFunc) CommandFunc
+)
+
+// Middlewarer is an abstraction of a plugin that provides middlewares.
+// If a plugin does not implement the interface, it will be assumed, that the
+// plugin does not provide any middlewares.
+type Middlewarer interface {
+	// Middlewares returns a copy of the MiddlewareFuncs of the plugin.
+	Middlewares() []MiddlewareFunc
+}
+
+// MiddlewareManager is a struct that can be embedded in commands and modules
+// to provide middleware capabilities.
+// It implements Middlewarer.
+//
+// MiddlewareManagers zero value is guaranteed to function properly.
+type MiddlewareManager struct {
+	middlewares []MiddlewareFunc
+	mutex       sync.RWMutex
+}
+
+// AddMiddleware adds the passed middleware to the MiddlewareManager.
+// If the middleware's type is invalid, AddMiddleware will return
+// ErrNotAMiddleware.
+//
+// Supported types are:
+//		• func(*state.State, interface{})
+//		• func(*state.State, interface{}) error
+//		• func(*state.State, *state.Base)
+//		• func(*state.State, *state.Base) error
+//		• func(*state.State, *state.MessageCreateEvent)
+//		• func(*state.State, *state.MessageCreateEvent) error
+//		• func(next CommandFunc) CommandFunc
+func (m *MiddlewareManager) AddMiddleware(f interface{}) error {
+	var mf MiddlewareFunc
+
+	switch f := f.(type) {
+	case func(*state.State, interface{}):
+		mf = func(next CommandFunc) CommandFunc {
+			return func(s *state.State, ctx *plugin.Context) (interface{}, error) {
+				f(s, ctx.MessageCreateEvent)
+				return next(s, ctx)
+			}
+		}
+	case func(*state.State, interface{}) error:
+		mf = func(next CommandFunc) CommandFunc {
+			return func(s *state.State, ctx *plugin.Context) (interface{}, error) {
+				err := f(s, ctx.MessageCreateEvent)
+				if err != nil {
+					return nil, err
+				}
+
+				return next(s, ctx)
+			}
+		}
+	case func(*state.State, *state.Base):
+		mf = func(next CommandFunc) CommandFunc {
+			return func(s *state.State, ctx *plugin.Context) (interface{}, error) {
+				f(s, ctx.MessageCreateEvent.Base)
+				return next(s, ctx)
+			}
+		}
+	case func(*state.State, *state.Base) error:
+		mf = func(next CommandFunc) CommandFunc {
+			return func(s *state.State, ctx *plugin.Context) (interface{}, error) {
+				err := f(s, ctx.MessageCreateEvent.Base)
+				if err != nil {
+					return nil, err
+				}
+
+				return next(s, ctx)
+			}
+		}
+	case func(*state.State, *state.MessageCreateEvent):
+		mf = func(next CommandFunc) CommandFunc {
+			return func(s *state.State, ctx *plugin.Context) (interface{}, error) {
+				f(s, ctx.MessageCreateEvent)
+				return next(s, ctx)
+			}
+		}
+	case func(*state.State, *state.MessageCreateEvent) error:
+		mf = func(next CommandFunc) CommandFunc {
+			return func(s *state.State, ctx *plugin.Context) (interface{}, error) {
+				err := f(s, ctx.MessageCreateEvent)
+				if err != nil {
+					return nil, err
+				}
+
+				return next(s, ctx)
+			}
+		}
+	case func(next CommandFunc) CommandFunc:
+		mf = f
+	case MiddlewareFunc:
+		mf = f
+	default:
+		return errors.WithStack(ErrNotAMiddleware)
+	}
+
+	m.mutex.Lock()
+	m.middlewares = append(m.middlewares, mf)
+	m.mutex.Unlock()
+
+	return nil
+}
+
+// MustAddMiddleware is the same as AddMiddleware, but panics if AddMiddleware
+// returns an error.
+func (m *MiddlewareManager) MustAddMiddleware(f interface{}) {
+	err := m.AddMiddleware(f)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Middlewares returns a copy of the middlewares of the manager.
+func (m *MiddlewareManager) Middlewares() (cp []MiddlewareFunc) {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
+	if len(m.middlewares) == 0 {
+		return nil
+	}
+
+	cp = make([]MiddlewareFunc, len(m.middlewares))
+	copy(cp, m.middlewares)
+
+	return
+}
---
+++ pkg/bot/middleware_test.go
@@ -0,0 +1,53 @@
+package bot
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/mavolin/disstate/v2/pkg/state"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/mavolin/adam/pkg/errors"
+	"github.com/mavolin/adam/pkg/plugin"
+)
+
+func TestMiddlewareManager_AddMiddleware(t *testing.T) {
+	successCases := []interface{}{
+		func(*state.State, interface{}) {},
+		func(*state.State, interface{}) error { return nil },
+		func(*state.State, *state.Base) {},
+		func(*state.State, *state.Base) error { return nil },
+		func(*state.State, *state.MessageCreateEvent) {},
+		func(*state.State, *state.MessageCreateEvent) error { return nil },
+		func(next CommandFunc) CommandFunc {
+			return func(*state.State, *plugin.Context) (interface{}, error) {
+				return nil, nil
+			}
+		},
+		MiddlewareFunc(func(next CommandFunc) CommandFunc {
+			return func(*state.State, *plugin.Context) (interface{}, error) {
+				return nil, nil
+			}
+		}),
+	}
+
+	t.Run("success", func(t *testing.T) {
+		for _, c := range successCases {
+			funcType := reflect.TypeOf(c)
+
+			t.Run(funcType.String(), func(t *testing.T) {
+				var m MiddlewareManager
+
+				err := m.AddMiddleware(c)
+				assert.NoError(t, err)
+			})
+		}
+	})
+
+	t.Run("failure", func(t *testing.T) {
+		var m MiddlewareManager
+
+		err := m.AddMiddleware("invalid")
+		assert.True(t, errors.Is(err, ErrNotAMiddleware))
+	})
+}
