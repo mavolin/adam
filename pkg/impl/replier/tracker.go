36500478b0ccc8a5feded2b74459d3e02e812bcc
mavolin <48887425+mavolin@users.noreply.github.com>
2020-10-15 18:19:56 +0200

    feat(replier): add Tracker

---
+++ pkg/impl/replier/tracker.go
@@ -0,0 +1,86 @@
+package replier
+
+import (
+	"sync"
+
+	"github.com/diamondburned/arikawa/api"
+	"github.com/diamondburned/arikawa/discord"
+	"github.com/mavolin/disstate/v2/pkg/state"
+)
+
+// Tracker is a plugin.Replier that tracks the messages that were sent.
+type Tracker struct {
+	s *state.State
+
+	dms     []discord.Message
+	dmMutex sync.RWMutex
+	dmID    discord.ChannelID
+	userID  discord.UserID
+
+	guildMessages      []discord.Message
+	guildMessagesMutex sync.RWMutex
+	guildChannelID     discord.ChannelID
+}
+
+// NewTracker creates a new tracker using the passed state, with the passed
+// invoking user and the passed guild channel.
+func NewTracker(s *state.State, invokingUserID discord.UserID, guildChannelID discord.ChannelID) *Tracker {
+	return &Tracker{
+		s:              s,
+		userID:         invokingUserID,
+		guildChannelID: guildChannelID,
+	}
+}
+
+// GuildMessages returns the guild messages that were sent.
+func (t *Tracker) GuildMessages() (cp []discord.Message) {
+	cp = make([]discord.Message, len(t.guildMessages))
+	copy(cp, t.guildMessages)
+
+	return
+}
+
+// DMs returns the direct messages that were sent.
+func (t *Tracker) DMs() (cp []discord.Message) {
+	cp = make([]discord.Message, len(t.dms))
+	copy(cp, t.dms)
+
+	return
+}
+
+func (t *Tracker) ReplyMessage(data api.SendMessageData) (*discord.Message, error) {
+	t.guildMessagesMutex.Lock()
+	defer t.guildMessagesMutex.Unlock()
+
+	msg, err := t.s.SendMessageComplex(t.guildChannelID, data)
+	if err != nil {
+		return nil, err
+	}
+
+	t.guildMessages = append(t.guildMessages, *msg)
+
+	return msg, nil
+}
+
+func (t *Tracker) ReplyDM(data api.SendMessageData) (*discord.Message, error) {
+	if !t.dmID.IsValid() {
+		c, err := t.s.CreatePrivateChannel(t.userID)
+		if err != nil {
+			return nil, err
+		}
+
+		t.dmID = c.ID
+	}
+
+	t.dmMutex.Lock()
+	defer t.dmMutex.Unlock()
+
+	msg, err := t.s.SendMessageComplex(t.dmID, data)
+	if err != nil {
+		return nil, err
+	}
+
+	t.dms = append(t.dms, *msg)
+
+	return msg, nil
+}
---
+++ pkg/impl/replier/tracker_test.go
@@ -0,0 +1,171 @@
+package replier
+
+import (
+	"testing"
+
+	"github.com/diamondburned/arikawa/api"
+	"github.com/diamondburned/arikawa/discord"
+	"github.com/mavolin/disstate/v2/pkg/state"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTracker_GuildMessages(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	var channelID discord.ChannelID = 123
+
+	r := NewTracker(s, 0, channelID)
+
+	data := api.SendMessageData{
+		Content: "abc",
+	}
+
+	expectMessage := discord.Message{
+		ID:        456,
+		ChannelID: channelID,
+		Author:    discord.User{ID: 789},
+		Content:   data.Content,
+	}
+
+	m.SendMessageComplex(data, expectMessage)
+
+	actualMessage, err := r.ReplyMessage(data)
+	require.NoError(t, err)
+	assert.Equal(t, expectMessage, *actualMessage)
+
+	expectGuildMessage := []discord.Message{expectMessage}
+
+	actualGuildMessages := r.GuildMessages()
+	assert.Equal(t, expectGuildMessage, actualGuildMessages)
+
+	m.Eval()
+}
+
+func TestTracker_DMs(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	var channelID discord.ChannelID = 123
+
+	r := &Tracker{
+		s:    s,
+		dmID: channelID,
+	}
+
+	data := api.SendMessageData{
+		Content: "abc",
+	}
+
+	expectMessage := discord.Message{
+		ID:        456,
+		ChannelID: channelID,
+		Author:    discord.User{ID: 789},
+		Content:   data.Content,
+	}
+
+	m.SendMessageComplex(data, expectMessage)
+
+	actualMessage, err := r.ReplyDM(data)
+	require.NoError(t, err)
+	assert.Equal(t, expectMessage, *actualMessage)
+
+	expectDMs := []discord.Message{expectMessage}
+
+	actualDMs := r.DMs()
+	assert.Equal(t, expectDMs, actualDMs)
+
+	m.Eval()
+}
+
+func TestTracker_ReplyMessage(t *testing.T) {
+	m, s := state.NewMocker(t)
+
+	var channelID discord.ChannelID = 123
+
+	r := NewTracker(s, 0, channelID)
+
+	data := api.SendMessageData{
+		Content: "abc",
+	}
+
+	expect := discord.Message{
+		ID:        456,
+		ChannelID: channelID,
+		Author:    discord.User{ID: 789},
+		Content:   data.Content,
+	}
+
+	m.SendMessageComplex(data, expect)
+
+	actual, err := r.ReplyMessage(data)
+	require.NoError(t, err)
+	assert.Equal(t, expect, *actual)
+
+	m.Eval()
+}
+
+func TestTracker_ReplyDM(t *testing.T) {
+	t.Run("unknown dm id", func(t *testing.T) {
+		m, s := state.NewMocker(t)
+
+		var (
+			channelID discord.ChannelID = 123
+			userID    discord.UserID    = 456
+		)
+
+		r := NewTracker(s, userID, 0)
+
+		data := api.SendMessageData{
+			Content: "abc",
+		}
+
+		expect := discord.Message{
+			ID:        789,
+			ChannelID: channelID,
+			Author:    discord.User{ID: userID},
+			Content:   data.Content,
+		}
+
+		m.CreatePrivateChannel(discord.Channel{
+			ID:           channelID,
+			DMRecipients: []discord.User{{ID: userID}},
+		})
+		m.SendMessageComplex(data, expect)
+
+		actual, err := r.ReplyDM(data)
+		require.NoError(t, err)
+		assert.Equal(t, expect, *actual)
+
+		m.Eval()
+	})
+
+	t.Run("known dm id", func(t *testing.T) {
+		m, s := state.NewMocker(t)
+
+		var channelID discord.ChannelID = 123
+
+		r := &Tracker{
+			s:    s,
+			dmID: channelID,
+		}
+
+		data := api.SendMessageData{
+			Content: "abc",
+		}
+
+		expect := discord.Message{
+			ID:        456,
+			ChannelID: channelID,
+			Author:    discord.User{ID: 789},
+			Content:   data.Content,
+		}
+
+		m.SendMessageComplex(data, expect)
+
+		actual, err := r.ReplyDM(data)
+		require.NoError(t, err)
+		assert.Equal(t, expect, *actual)
+
+		m.Eval()
+	})
+}
