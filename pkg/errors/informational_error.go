8669d4d5a74afd0b0effba1b6803d62be5e7e45e
mavolin <48887425+mavolin@users.noreply.github.com>
2020-10-15 20:30:29 +0200

    feat(plugin): make ErrInsufficientSendPermissions of type errors.InformationalError

--- pkg/errors/informational_error.go
+++ pkg/errors/informational_error.go
@@ -3,6 +3,8 @@ package errors
 import (
 	"github.com/mavolin/disstate/v2/pkg/state"
 
+	_ "unsafe" // for go:linkname
+
 	"github.com/mavolin/adam/pkg/plugin"
 )
 
@@ -12,8 +14,26 @@ import (
 //
 // It is intended to be used, if the user signals to cancel a command early
 // and is therefore just a signaling error, rather than an actual exception.
-var Abort error = &InformationalError{
-	s: "abort",
+var Abort error = NewInformationalError("abort")
+
+func init() {
+	// Hack to make plugin.ErrInsufficientSendPermissions of type
+	// InformationalError without creating an import cycle.
+	//
+	// We safely do this, since the error, unreplaced, is of type
+	// errors.errorString, a type that can't be type asserted.
+	// This means no code can be written that works based on the old type.
+	// As soon as a type check for the replaced error is performed, the init
+	// function will have already run during imports and everything will work
+	// fine as well.
+	// And since direct/pointer comparisons are unaffected by this, there won't
+	// be an issue using that either.
+	// Hence, no faulty code can be written.
+	//
+	// I know this is not pretty, but still better than copying this type,
+	// using that type instead, and having the end user handle the error
+	// specially.
+	plugin.ErrInsufficientSendPermissions = NewInformationalError("insufficient permissions to send message")
 }
 
 // InformationalError is an error that won't be handled.
