aaf71fbbb7f14a9f32b791906946e04d35c30c42
mavolin <48887425+mavolin@users.noreply.github.com>
2020-10-17 16:13:15 +0200

    feat(arg): add Text

---
+++ pkg/impl/arg/text_types_test.go
@@ -0,0 +1,152 @@
+package arg
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/mavolin/adam/pkg/errors"
+	"github.com/mavolin/adam/pkg/i18n"
+)
+
+func TestText_Parse(t *testing.T) {
+	sucessCases := []struct {
+		name string
+		text Text
+
+		raw string
+
+		expect string
+	}{
+		{
+			name:   "simple text",
+			text:   SimpleText,
+			raw:    "abc",
+			expect: "abc",
+		},
+		{
+			name:   "min length",
+			text:   Text{MinLength: 3},
+			raw:    "abc",
+			expect: "abc",
+		},
+		{
+			name:   "max length",
+			text:   Text{MaxLength: 3},
+			raw:    "abc",
+			expect: "abc",
+		},
+		{
+			name:   "regexp",
+			text:   Text{Regexp: regexp.MustCompile("abc")},
+			raw:    "abc",
+			expect: "abc",
+		},
+	}
+
+	t.Run("success", func(t *testing.T) {
+		for _, c := range sucessCases {
+			t.Run(c.name, func(t *testing.T) {
+				ctx := &Context{Raw: c.raw}
+
+				actual, err := c.text.Parse(nil, ctx)
+				require.NoError(t, err)
+				assert.Equal(t, c.expect, actual)
+			})
+		}
+	})
+
+	failureCases := []struct {
+		name string
+		text Text
+
+		raw string
+
+		expectArg, expectFlag i18n.Config
+	}{
+		{
+			name: "below min",
+			text: Text{MinLength: 3},
+			raw:  "ab",
+			expectArg: textBelowMinLengthErrorArg.
+				WithPlaceholders(map[string]interface{}{
+					"min": uint(3),
+				}),
+			expectFlag: textBelowMinLengthErrorFlag.
+				WithPlaceholders(map[string]interface{}{
+					"min": uint(3),
+				}),
+		},
+		{
+			name: "above max",
+			text: Text{MaxLength: 3},
+			raw:  "abcd",
+			expectArg: textAboveMaxLengthErrorArg.
+				WithPlaceholders(map[string]interface{}{
+					"max": uint(3),
+				}),
+			expectFlag: textAboveMaxLengthErrorFlag.
+				WithPlaceholders(map[string]interface{}{
+					"max": uint(3),
+				}),
+		},
+		{
+			name: "regexp not matching",
+			text: Text{Regexp: regexp.MustCompile("abc")},
+			raw:  "def",
+			expectArg: regexpNotMatchingErrorArg.
+				WithPlaceholders(map[string]interface{}{
+					"regexp": "abc",
+				}),
+			expectFlag: regexpNotMatchingErrorFlag.
+				WithPlaceholders(map[string]interface{}{
+					"regexp": "abc",
+				}),
+		},
+		{
+			name: "regexp not matching - custom error",
+			text: Text{
+				Regexp:          regexp.MustCompile("abc"),
+				RegexpErrorArg:  i18n.NewFallbackConfig("arg", "arg"),
+				RegexpErrorFlag: i18n.NewFallbackConfig("flag", "flag"),
+			},
+			raw: "def",
+			expectArg: i18n.NewFallbackConfig("arg", "arg").
+				WithPlaceholders(map[string]interface{}{
+					"regexp": "abc",
+				}),
+			expectFlag: i18n.NewFallbackConfig("flag", "flag").
+				WithPlaceholders(map[string]interface{}{
+					"regexp": "abc",
+				}),
+		},
+	}
+
+	t.Run("failure", func(t *testing.T) {
+		for _, c := range failureCases {
+			t.Run(c.name, func(t *testing.T) {
+				ctx := &Context{
+					Raw:  c.raw,
+					Kind: KindArg,
+				}
+
+				c.expectArg.Placeholders = attachDefaultPlaceholders(c.expectArg.Placeholders, ctx)
+
+				_, actual := c.text.Parse(nil, ctx)
+				assert.Equal(t, errors.NewArgumentParsingErrorl(c.expectArg), actual)
+
+				ctx = &Context{
+					Raw:  c.raw,
+					Kind: KindFlag,
+				}
+
+				c.expectFlag.Placeholders = attachDefaultPlaceholders(c.expectFlag.Placeholders, ctx)
+
+				_, actual = c.text.Parse(nil, ctx)
+				assert.Equal(t, errors.NewArgumentParsingErrorl(c.expectFlag), actual)
+			})
+		}
+	})
+}
--- pkg/impl/arg/type_terms.go
+++ pkg/impl/arg/type_terms.go
@@ -3,88 +3,125 @@ package arg
 import "github.com/mavolin/adam/pkg/i18n"
 
 // =============================================================================
+// Common
+// =====================================================================================
+
+// ================================ Errors ================================
+
+var (
+	regexpNotMatchingErrorArg = i18n.NewFallbackConfig(
+		"args.types.common.errors.regexp_not_matching.arg",
+		"Argument {{.position}} must match `{{.regexp}}`.")
+	regexpNotMatchingErrorFlag = i18n.NewFallbackConfig(
+		"args.types.common.errors.regexp_not_matching.flag",
+		"The `-{{.used_name}}`-flag must match `{{.regexp}}`.")
+)
+
+// =============================================================================
 // Switch
 // =====================================================================================
 
-var (
-	// ================================ Meta Data ================================
+// ================================ Meta Data ================================
 
+var (
 	switchName        = i18n.NewFallbackConfig("args.types.switch.name", "Switch")
 	switchDescription = i18n.NewFallbackConfig(
 		"args.types.switch.description",
 		"Used to turn on a feature of a command. Only used with flags.")
-
-	// ================================ Errors ================================
-
-	switchWithContentError = i18n.NewFallbackConfig(
-		"args.types.switch.errors.with_content", "`-{{.name}}` is a Switch flag and cannot be used with content.")
 )
 
-type (
-	// ================================ Errors ================================
-	switchWithContentErrorPlaceholders struct {
-		Name string
-	}
-)
+// ================================ Errors ================================
+
+var switchWithContentError = i18n.NewFallbackConfig(
+	"args.types.switch.errors.with_content", "`-{{.name}}` is a Switch flag and cannot be used with content.")
+
+type switchWithContentErrorPlaceholders struct {
+	Name string
+}
 
 // =============================================================================
 // Numbers
 // =====================================================================================
 
+// ================================ Integer Meta Data ================================
+
 var (
-	// ================================ Integer Meta Data ================================
-
 	integerName        = i18n.NewFallbackConfig("args.types.integer.name", "Integer")
-	integerDescription = i18n.NewFallbackConfig(
-		"args.types.integer.description",
-		"A whole number.")
-
-	// ================================ Decimal Meta Data ================================
-
-	decimalName        = i18n.NewFallbackConfig("args.types.decimal.name", "Decimal")
-	decimalDescription = i18n.NewFallbackConfig(
-		"args.types.decimal.description",
-		"A decimal number.")
+	integerDescription = i18n.NewFallbackConfig("args.types.integer.description", "A whole number.")
 )
 
+// ================================ Decimal Meta Data ================================
+
 var (
-	// ================================ Integer Errors ================================
+	decimalName        = i18n.NewFallbackConfig("args.types.decimal.name", "Decimal")
+	decimalDescription = i18n.NewFallbackConfig("args.types.decimal.description", "A decimal number.")
+)
 
-	integerSyntaxError = i18n.NewFallbackConfig(
-		"args.types.integer.errors.syntax.argument", "{{.raw}} is not an integer.")
+// ================================ Integer Errors ================================
 
-	// ================================ Decimal Errors ================================
+var integerSyntaxError = i18n.NewFallbackConfig("args.types.integer.errors.syntax", "{{.raw}} is not an integer.")
 
-	decimalSyntaxError = i18n.NewFallbackConfig(
-		"args.types.integer.errors.syntax.argument", "{{.raw}} is not an integer.")
+// ================================ Decimal Errors ================================
 
-	// ================================ Shared Errors ================================
+var decimalSyntaxError = i18n.NewFallbackConfig("args.types.decimal.errors.syntax", "{{.raw}} is not a decimal.")
 
+// ================================ Shared Errors ================================
+
+var (
 	numberUnderRangeErrorArg = i18n.NewFallbackConfig(
-		"args.types.number.errors.under_range.argument",
+		"args.types.number.errors.under_range.arg",
 		"{{.raw}} is too small, try using a larger number as argument {{.position}}.")
 	numberUnderRangeErrorFlag = i18n.NewFallbackConfig(
 		"args.types.number.errors.under_range.flag",
 		"{{.raw}} is too small, try giving the `-{{.used_name}}`-flag a larger number.")
 
 	numberOverRangeErrorArg = i18n.NewFallbackConfig(
-		"args.types.number.errors.over_range.argument",
+		"args.types.number.errors.over_range.arg",
 		"{{.raw}} is too large, try using a smaller number as argument {{.position}}.")
 	numberOverRangeErrorFlag = i18n.NewFallbackConfig(
 		"args.types.integer.errors.over_range.flag",
-		"{{.raw}} is a bit too large, try giving the `-{{.used_name}}`-flag a smaller number.")
+		"{{.raw}} is a too large, try giving the `-{{.used_name}}`-flag a smaller number.")
 
 	numberBelowMinErrorArg = i18n.NewFallbackConfig(
-		"args.types.number.errors.below_min.argument",
+		"args.types.number.errors.below_min.arg",
 		"Argument {{.position}} must be larger or equal to {{.min}}.")
 	numberBelowMinErrorFlag = i18n.NewFallbackConfig(
 		"args.types.number.errors.below_min.flag",
 		"The `-{{.used_name}}`-flag must be larger or equal to {{.min}}.")
 
 	numberAboveMaxErrorArg = i18n.NewFallbackConfig(
-		"args.types.number.errors.below_min.argument",
+		"args.types.number.errors.below_min.arg",
 		"Argument {{.position}} must be smaller or equal to {{.max}}.")
 	numberAboveMaxErrorFlag = i18n.NewFallbackConfig(
 		"args.types.number.errors.below_min.flag",
 		"The `-{{.used_name}}`-flag must be smaller or equal to {{.max}}.")
+)
+
+// =============================================================================
+// Text
+// =====================================================================================
+
+// ================================ Meta Data ================================
+
+var (
+	textName        = i18n.NewFallbackConfig("args.types.text.name", "Text")
+	textDescription = i18n.NewFallbackConfig("args.types.text.desc", "A text. What else is there to say.")
+)
+
+// ================================ Errors ================================
+
+var (
+	textBelowMinLengthErrorArg = i18n.NewFallbackConfig(
+		"args.types.text.errors.below_min_length.arg",
+		"The text in argument {{.position}} must be at least {{.min}} characters long.")
+	textBelowMinLengthErrorFlag = i18n.NewFallbackConfig(
+		"args.types.text.errors.below_min_length.flag",
+		"The text used in the `-{{.used_name}}`-flag must be at least {{.min}} characters long.")
+
+	textAboveMaxLengthErrorArg = i18n.NewFallbackConfig(
+		"args.types.text.errors.above_max_length.arg",
+		"The text in argument {{.position}} may not be longer than {{.max}} characters.")
+	textAboveMaxLengthErrorFlag = i18n.NewFallbackConfig(
+		"args.types.text.errors.above_max_length.flag",
+		"The text used in the `-{{.used_name}}`-flag may not be longer than {{.max}} characters.")
 )
