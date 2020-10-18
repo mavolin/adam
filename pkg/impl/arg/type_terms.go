107f02e216f2937e1e9a3ec9a5747adbe8eba9e4
mavolin <48887425+mavolin@users.noreply.github.com>
2020-10-18 22:06:39 +0200

    feat(arg): add RegularExpression

--- pkg/impl/arg/type_terms.go
+++ pkg/impl/arg/type_terms.go
@@ -14,7 +14,7 @@ var (
 		"Argument {{.position}} must match `{{.regexp}}`.")
 	regexpNotMatchingErrorFlag = i18n.NewFallbackConfig(
 		"args.types.common.errors.regexp_not_matching.flag",
-		"The `-{{.used_name}}`-flag must match `{{.regexp}}`.")
+		"The `{{.used_name}}`-flag must match `{{.regexp}}`.")
 )
 
 // =============================================================================
@@ -33,7 +33,7 @@ var (
 // ================================ Errors ================================
 
 var switchWithContentError = i18n.NewFallbackConfig(
-	"args.types.switch.errors.with_content", "`-{{.name}}` is a Switch flag and cannot be used with content.")
+	"args.types.switch.errors.with_content", "`{{.name}}` is a Switch flag and cannot be used with content.")
 
 type switchWithContentErrorPlaceholders struct {
 	Name string
@@ -73,28 +73,28 @@ var (
 		"{{.raw}} is too small, try using a larger number as argument {{.position}}.")
 	numberUnderRangeErrorFlag = i18n.NewFallbackConfig(
 		"args.types.number.errors.under_range.flag",
-		"{{.raw}} is too small, try giving the `-{{.used_name}}`-flag a larger number.")
+		"{{.raw}} is too small, try giving the `{{.used_name}}`-flag a larger number.")
 
 	numberOverRangeErrorArg = i18n.NewFallbackConfig(
 		"args.types.number.errors.over_range.arg",
 		"{{.raw}} is too large, try using a smaller number as argument {{.position}}.")
 	numberOverRangeErrorFlag = i18n.NewFallbackConfig(
 		"args.types.integer.errors.over_range.flag",
-		"{{.raw}} is a too large, try giving the `-{{.used_name}}`-flag a smaller number.")
+		"{{.raw}} is a too large, try giving the `{{.used_name}}`-flag a smaller number.")
 
 	numberBelowMinErrorArg = i18n.NewFallbackConfig(
 		"args.types.number.errors.below_min.arg",
 		"Argument {{.position}} must be larger or equal to {{.min}}.")
 	numberBelowMinErrorFlag = i18n.NewFallbackConfig(
 		"args.types.number.errors.below_min.flag",
-		"The `-{{.used_name}}`-flag must be larger or equal to {{.min}}.")
+		"The `{{.used_name}}`-flag must be larger or equal to {{.min}}.")
 
 	numberAboveMaxErrorArg = i18n.NewFallbackConfig(
 		"args.types.number.errors.below_min.arg",
 		"Argument {{.position}} must be smaller or equal to {{.max}}.")
 	numberAboveMaxErrorFlag = i18n.NewFallbackConfig(
 		"args.types.number.errors.below_min.flag",
-		"The `-{{.used_name}}`-flag must be smaller or equal to {{.max}}.")
+		"The `{{.used_name}}`-flag must be smaller or equal to {{.max}}.")
 )
 
 // =============================================================================
@@ -116,14 +116,14 @@ var (
 		"The text in argument {{.position}} must be at least {{.min}} characters long.")
 	textBelowMinLengthErrorFlag = i18n.NewFallbackConfig(
 		"args.types.text.errors.below_min_length.flag",
-		"The text used in the `-{{.used_name}}`-flag must be at least {{.min}} characters long.")
+		"The text used in the `{{.used_name}}`-flag must be at least {{.min}} characters long.")
 
 	textAboveMaxLengthErrorArg = i18n.NewFallbackConfig(
 		"args.types.text.errors.above_max_length.arg",
 		"The text in argument {{.position}} may not be longer than {{.max}} characters.")
 	textAboveMaxLengthErrorFlag = i18n.NewFallbackConfig(
 		"args.types.text.errors.above_max_length.flag",
-		"The text used in the `-{{.used_name}}`-flag may not be longer than {{.max}} characters.")
+		"The text used in the `{{.used_name}}`-flag may not be longer than {{.max}} characters.")
 )
 
 // =============================================================================
@@ -143,7 +143,7 @@ var (
 	linkInvalidErrorArg = i18n.NewFallbackConfig(
 		"args.types.link.errors.invalid.arg", "Argument {{.position}} must be a valid link.")
 	linkInvalidErrorFlag = i18n.NewFallbackConfig(
-		"args.types.link.errors.invalid.flag", "The `-{{.used.name}}`-flag must be a valid link.")
+		"args.types.link.errors.invalid.flag", "The `{{.used.name}}`-flag must be a valid link.")
 )
 
 // =============================================================================
@@ -165,21 +165,21 @@ var (
 		"Argument {{.position}} must be at least {{.min}} characters long.")
 	idBelowMinLengthErrorFlag = i18n.NewFallbackConfig(
 		"args.types.id.errors.below_min_length.flag",
-		"The `-{{.used_name}}`-flag must be at least {{.min}} characters long.")
+		"The `{{.used_name}}`-flag must be at least {{.min}} characters long.")
 
 	idAboveMaxLengthErrorArg = i18n.NewFallbackConfig(
 		"args.types.id.errors.above_max_length.arg",
 		"Argument {{.position}} may not be longer than {{.max}} characters.")
 	idAboveMaxLengthErrorFlag = i18n.NewFallbackConfig(
 		"args.types.id.errors.above_max_length.flag",
-		"The `-{{.used_name}}`-flag may not be longer than {{.max}} characters.")
+		"The `{{.used_name}}`-flag may not be longer than {{.max}} characters.")
 
 	idNotANumberErrorArg = i18n.NewFallbackConfig(
 		"args.types.id.errors.not_a_number.arg",
 		"Argument {{.position}} must be a number.")
 	idNotANumberErrorFlag = i18n.NewFallbackConfig(
 		"args.types.id.errors.not_a_number.flag",
-		"The `-{{.used_name}}`-flag must be a number.")
+		"The `{{.used_name}}`-flag must be a number.")
 )
 
 // =============================================================================
@@ -202,7 +202,7 @@ var (
 	choiceInvalidErrorArg = i18n.NewFallbackConfig(
 		"args.types.choice.errors.invalid.arg", "`{{.raw}}` is not a valid choice for argument {{.position}}.")
 	choiceInvalidErrorFlag = i18n.NewFallbackConfig(
-		"args.types.choice.errors.invalid.flag", "`{{.raw}}` is not a valid choice for the `-{{.{{.used_name}}`-flag.")
+		"args.types.choice.errors.invalid.flag", "`{{.raw}}` is not a valid choice for the `{{.used_name}}`-flag.")
 )
 
 // =============================================================================
@@ -257,7 +257,7 @@ var (
 		"The mention in argument {{.position}} is invalid. Make sure the user is still on the server.")
 	userInvalidMentionFlag = i18n.NewFallbackConfig(
 		"args.types.user.errors.invalid_mention.flag",
-		"The mention in the `-{{.used_name}}`-flag is invalid. Make sure the user is still on the server.")
+		"The mention in the `{{.used_name}}`-flag is invalid. Make sure the user is still on the server.")
 )
 
 // =============================================================================
@@ -281,5 +281,136 @@ var (
 	userInvalidIDArg = i18n.NewFallbackConfig(
 		"args.types.user.errors.invalid_id.arg", "The user id in argument {{.position}} is invalid.")
 	userInvalidIDFlag = i18n.NewFallbackConfig(
-		"args.types.user.errors.invalid_id.flag", "The user id in the `-{{.used_name}}`-flag is invalid.")
+		"args.types.user.errors.invalid_id.flag", "The user id in the `{{.used_name}}`-flag is invalid.")
+)
+
+// =============================================================================
+// RegularExpression
+// =====================================================================================
+
+// ================================ Meta Data ================================
+
+var (
+	regexpName        = i18n.NewFallbackConfig("args.types.regular_expression.name", "Regular Expression")
+	regexpDescription = i18n.NewFallbackConfig(
+		"args.types.regular_expression.description",
+		"A regular expression is a regular expression following the RE2/Golang-flavor. "+
+			"It can be used to macht text that follows user-defined rules.")
+)
+
+// ================================ Errors ================================
+
+var (
+	regexpInvalidErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid.arg",
+		"The regular expression in argument {{.position}} is invalid.")
+	regexpInvalidErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag is invalid.")
+
+	regexpInvalidCharClassErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_character_class.arg",
+		"The regular expression in argument {{.position}} uses an invalid character class:\n```\n{{.expression}}\n```")
+	regexpInvalidCharClassErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_character_class.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag uses an invalid character class:"+
+			"\n```\n{{.expression}}\n```")
+
+	regexpInvalidCharRangeErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_character_range.arg",
+		"The regular expression in argument {{.position}} uses an invalid character class range:\n"+
+			"```\n{{.expression}}\n```")
+	regexpInvalidCharRangeErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_character_range.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag uses an invalid character class range:\n"+
+			"```\n{{.expression}}\n```")
+
+	regexpInvalidEscapeErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_escape.arg",
+		"The regular expression in argument {{.position}} uses an invalid escape sequence:\n```\n{{.expression}}\n```")
+	regexpInvalidEscapeErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_escape.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag uses an invalid escape sequence\n"+
+			"```\n{{.expression}}\n```")
+
+	regexpInvalidNamedCaptureErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_named_capture.arg",
+		"The regular expression in argument {{.position}} uses an invalid named capture:\n```\n{{.expression}}\n```")
+	regexpInvalidNamedCaptureErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_named_capture.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag uses an invalid named capture:\n"+
+			"```\n{{.expression}}\n```")
+
+	regexpInvalidPerlOpErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_perl_operation.arg",
+		"The regular expression in argument {{.position}} uses invalid or unsupported Perl syntax:\n"+
+			"```\n{{.expression}}\n```")
+	regexpInvalidPerlOpErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_perl_operation.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag uses invalid or unsupported Perl syntax:\n"+
+			"```\n{{.expression}}\n```")
+
+	regexpInvalidRepeatOpErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_repeat_operation.arg",
+		"The regular expression in argument {{.position}} has two consecutive `+`.")
+	regexpInvalidRepeatOpErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_repeat_operation.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag has two consecutive `+`.")
+
+	regexpInvalidRepeatSizeErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_repeat_size.arg",
+		"The regular expression in argument {{.position}} uses an invalid invalid repeat count:\n"+
+			"```\n{{.expression}}\n```")
+	regexpInvalidRepeatSizeErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_repeat_size.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag uses an invalid invalid repeat count:\n"+
+			"```\n{{.expression}}\n```")
+
+	regexpInvalidUTF8ErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_utf8.arg",
+		"The regular expression in argument {{.position}} uses invalid UTF-8:\n```\n{{.expression}}\n```")
+	regexpInvalidUTF8ErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.invalid_utf8.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag uses invalid UTF-8:\n```\n{{.expression}}\n```")
+
+	regexpMissingBracketErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.missing_bracket.arg",
+		"The regular expression in argument {{.position}} is missing a closing `]`:\n```\n{{.expression}}\n```")
+	regexpMissingBracketErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.missing_bracket.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag is missing a closing `]`:\n"+
+			"```\n{{.expression}}\n```")
+
+	regexpMissingParenErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.missing_parentheses.arg",
+		"The regular expression in argument {{.position}} is missing a closing `)`:\n```\n{{.expression}}\n```")
+	regexpMissingParenErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.missing_parentheses.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag is missing a closing `)`:\n"+
+			"```\n{{.expression}}\n```")
+
+	regexpMissingRepeatArgErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.missing_repeat_argument.arg",
+		"The regular expression in argument {{.position}} is missing an argument to the repetition operator:\n"+
+			"```\n{{.expression}}\n```")
+	regexpMissingRepeatArgErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.missing_repeat_argument.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag is missing an argument to the repetition "+
+			"operator:\n```\n{{.expression}}\n```")
+
+	regexpTrailingBackslashErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.trailing_backslash.arg",
+		"The regular expression in argument {{.position}} has a trailing backlash at the end of the expression.")
+	regexpTrailingBackslashErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.trailing_backslash.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag has a trailing backlash at the end of the "+
+			"expression.")
+
+	regexpUnexpectedParenErrorArg = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.unexpected_parentheses.arg",
+		"The regular expression in argument {{.position}} has an unexpected `)`:\n```\n{{.expression}}\n```")
+	regexpUnexpectedParenErrorFlag = i18n.NewFallbackConfig(
+		"args.types.regular_expression.errors.unexpected_parentheses.flag",
+		"The regular expression you used in the `{{.used_name}}`-flag has an unexpected `)`:"+
+			"\n```\n{{.expression}}\n```")
 )
