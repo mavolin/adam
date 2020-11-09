6d9453d4b32e3adc77145e95603e5b523760690e
mavolin <48887425+mavolin@users.noreply.github.com>
2020-11-09 19:49:29 +0100

    feat(arg): add Code

---
+++ pkg/impl/arg/code_type.go
@@ -0,0 +1,82 @@
+package arg
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/mavolin/disstate/v2/pkg/state"
+
+	"github.com/mavolin/adam/pkg/i18n"
+)
+
+// Code is the type used for code enclosed in a markdown code block.
+// Single, double, and triple backticks are permitted.
+//
+// Go type: *CodeBlock
+var Code Type = new(code)
+
+type code struct{}
+
+// CodeBlock is the type returned by Code.
+type CodeBlock struct {
+	// Language is the language the user specified, if any.
+	Language string
+	// Code is the code itself.
+	// Language and backticks are removed.
+	Code string
+	// QtyBackticks is the number of backticks the user used.
+	// Guaranteed to be 1, 2, or 3 in error != nil scenarios.
+	QtyBackticks int
+}
+
+func (c code) Name(l *i18n.Localizer) string {
+	name, _ := l.Localize(codeName) // we have a fallback
+	return name
+}
+
+func (c code) Description(l *i18n.Localizer) string {
+	desc, _ := l.Localize(codeDescription) // we have a fallback
+	return desc
+}
+
+var (
+	// these regexps aren't perfect (e.g. they allow ```a``), but they should cover
+	// most cases
+	singleBacktickRegexp = regexp.MustCompile(`^\x60(?P<code>[^\x60]+)\x60$`)
+	doubleBacktickRegexp = regexp.MustCompile(`^\x60\x60(?P<code>(?:\x60[^\x60]|[^\x60])+)\x60\x60$`)
+	tripleBacktickRegexp = regexp.MustCompile(
+		`^\x60\x60\x60(?:(?P<lang>\S+)\n)?(?P<code>(?:\x60\x60[^\x60]|\x60[^\x60]|[^\x60])+)\x60\x60\x60$`)
+)
+
+func (c code) Parse(_ *state.State, ctx *Context) (interface{}, error) {
+	if matches := singleBacktickRegexp.FindStringSubmatch(ctx.Raw); len(matches) >= 2 {
+		return &CodeBlock{
+			Code:         strings.Trim(matches[1], "\n"),
+			QtyBackticks: 1,
+		}, nil
+	} else if matches := doubleBacktickRegexp.FindStringSubmatch(ctx.Raw); len(matches) >= 2 {
+		return &CodeBlock{
+			Code:         strings.Trim(matches[1], "\n"),
+			QtyBackticks: 2,
+		}, nil
+	} else if matches := tripleBacktickRegexp.FindStringSubmatch(ctx.Raw); len(matches) >= 2 {
+		if len(matches) >= 3 { // with language
+			return &CodeBlock{
+				Language:     matches[1],
+				Code:         strings.Trim(matches[2], "\n"),
+				QtyBackticks: 3,
+			}, nil
+		}
+
+		return &CodeBlock{
+			Code:         strings.Trim(matches[1], "\n"),
+			QtyBackticks: 3,
+		}, nil
+	}
+
+	return nil, newArgParsingErr2(codeInvalidArg, codeInvalidFlag, ctx, nil)
+}
+
+func (c code) Default() interface{} {
+	return (*CodeBlock)(nil)
+}
