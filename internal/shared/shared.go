6f555e3b36ab7dc18295e98d04323358f86b722f
mavolin <48887425+mavolin@users.noreply.github.com>
2021-01-11 00:29:54 +0100

    docs(shared): comply with godoc formatting directives

--- internal/shared/shared.go
+++ internal/shared/shared.go
@@ -3,12 +3,14 @@ package shared
 
 import "github.com/mavolin/adam/pkg/utils/embedutil"
 
-// see errors.SetErrorEmbed and errors.NewErrorEmbed for more information.
+// ErrorEmbed is the shared *embedutil.Builder.
+// See errors.SetErrorEmbed and errors.NewErrorEmbed for more information.
 var ErrorEmbed = embedutil.NewBuilder().
 	WithSimpleTitlel(errorTitle).
 	WithColor(0xff5a5a)
 
-// see errors.SetInfoEmbed and errors.NewInfoEmbed for more information.
+// InfoEmbed is the shared info *embedutil.Builder.
+// See errors.SetInfoEmbed and errors.NewInfoEmbed for more information.
 var InfoEmbed = embedutil.NewBuilder().
 	WithSimpleTitlel(infoTitle).
 	WithColor(0x6eb7b1)
