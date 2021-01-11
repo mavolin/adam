1bd5499595f72145a80a460313463a9ddb759582
mavolin <48887425+mavolin@users.noreply.github.com>
2021-01-12 00:38:00 +0100

    refactor(bot): fix typo in unknown command error

--- pkg/bot/terms.go
+++ pkg/bot/terms.go
@@ -4,4 +4,4 @@ import "github.com/mavolin/adam/pkg/i18n"
 
 var unknownCommandErrorDescription = i18n.NewFallbackConfig(
 	"bot.errors.unknown_command.description",
-	"I don't know a command with that name")
+	"I don't know a command with that name.")
