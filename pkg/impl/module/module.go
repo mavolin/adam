ca7fe49931139da634ebfbf00bb15216eeb064f3
mavolin <48887425+mavolin@users.noreply.github.com>
2021-01-12 01:27:27 +0100

    feat(module): use a constructor function for creating Modules

--- pkg/impl/module/module.go
+++ pkg/impl/module/module.go
@@ -4,18 +4,24 @@ package module
 
 import (
 	"github.com/mavolin/adam/pkg/bot"
-	"github.com/mavolin/adam/pkg/i18n"
 	"github.com/mavolin/adam/pkg/plugin"
 )
 
 // Module is an implementation of plugin.Module with support for middlewares.
 type Module struct {
-	Meta plugin.ModuleMeta
-
-	bot.MiddlewareManager
+	plugin.ModuleMeta
+	*bot.MiddlewareManager
 
 	commands []plugin.Command
 	modules  []plugin.Module
+}
+
+// New creates a new *Module using the passed plugin.ModuleMeta.
+func New(meta plugin.ModuleMeta) *Module {
+	return &Module{
+		ModuleMeta:        meta,
+		MiddlewareManager: new(bot.MiddlewareManager),
+	}
 }
 
 // AddCommands adds the passed command to the module.
@@ -27,17 +33,3 @@ func (m *Module) AddCommand(cmd plugin.Command) {
 func (m *Module) AddModule(mod plugin.Module) {
 	m.modules = append(m.modules, mod)
 }
-
-func (m *Module) GetName() string                              { return m.Meta.GetName() }
-func (m *Module) GetShortDescription(l *i18n.Localizer) string { return m.Meta.GetShortDescription(l) }
-func (m *Module) GetLongDescription(l *i18n.Localizer) string  { return m.Meta.GetLongDescription(l) }
-func (m *Module) IsHidden() bool                               { return m.Meta.IsHidden() }
-func (m *Module) GetDefaultChannelTypes() plugin.ChannelTypes  { return m.Meta.GetDefaultChannelTypes() }
-
-func (m *Module) GetDefaultRestrictionFunc() plugin.RestrictionFunc {
-	return m.Meta.GetDefaultRestrictionFunc()
-}
-
-func (m *Module) GetDefaultThrottler() plugin.Throttler { return m.Meta.GetDefaultThrottler() }
-func (m *Module) Commands() []plugin.Command            { return m.commands }
-func (m *Module) Modules() []plugin.Module              { return m.modules }
