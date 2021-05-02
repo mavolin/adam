cfa1f03fe9626e0cf917ed694bec97b40d89b19c
mavolin <48887425+mavolin@users.noreply.github.com>
2021-05-02 12:36:21 +0200

    docs(errors): clarify that SetErrorEmbed and SetInfoEmbed are not concurrent safe

--- pkg/errors/embeds.go
+++ pkg/errors/embeds.go
@@ -11,6 +11,9 @@ import (
 // It should be made sure, that Builder always succeeds in building, as
 // otherwise errors might not get sent.
 // This means if localizing the Embed, fallbacks should be defined.
+//
+// SetErrorEmbed is not safe for concurrent use and should not be called after
+// the bot has been started.
 func SetErrorEmbed(b *embedutil.Builder) {
 	shared.ErrorEmbed = b
 }
@@ -26,6 +29,9 @@ func NewErrorEmbed() *embedutil.Builder {
 // It should be made sure, that Embed builder always succeeds in building, as
 // otherwise errors might not get sent.
 // This means if localizing the Embed, fallbacks should be defined.
+//
+// SetInfoEmbed is not safe for concurrent use and should not be called after
+// the bot has been started.
 func SetInfoEmbed(b *embedutil.Builder) {
 	shared.ErrorEmbed = b
 }
